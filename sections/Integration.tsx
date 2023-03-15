7815c58d71bda1f2ff0f89a150f82c7e264a119d
Giuliano <giuliano@madesa.com>
2023-03-15 20:11:34 -0300

    initial templates for Deco LandingPage

--- sections/Integration.tsx
+++ sections/Integration.tsx
@@ -10,8 +10,8 @@ export default function Integration({mainText, secondText}: Props) {
   return (
     <section class="bg-[#053535]">
       <div class="flex flex-col px-3 gap-10 md:px-[7rem]  pt-4 pb-12 md:pb-32 max-w-screen-2xl m-auto">
-        <p class='font-sans not-italic font-bold text-5xl text-white md:text-9xl' dangerouslySetInnerHTML={{ __html: mainText }}></p>
-        <p class="font-sans not-italic font-normal text-3xl text-white opacity-[67%] md:text-2xl md:w-3/5">
+        <p class='font-sans not-italic font-bold text-5xl text-white text-[56px] md:text-[128.94px] leading-[45px] md:leading-[105.5px] tracking[-1%]' dangerouslySetInnerHTML={{ __html: mainText }}></p>
+        <p class="font-sans not-italic font-normal text-[32px] leading-[38px] text-white opacity-[67%] md:w-3/5">
           {secondText}
         </p>
       </div>
