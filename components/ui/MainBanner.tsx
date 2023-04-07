33044607930476c1df86c9155c04771593d5b68a
Abraão Morais <moraisabraao@gmail.com>
2023-04-07 20:55:10 -0300

    add banner with theme v1

--- components/ui/MainBanner.tsx
+++ components/ui/MainBanner.tsx
@@ -1,24 +1,44 @@
-import QuillText from "deco-sites/starting/components/ui/QuillText.tsx";
-import type { HTML } from "deco-sites/std/components/types.ts";
+import Icon from "$start/components/ui/Icon.tsx";
+import Image from "deco-sites/std/components/Image.tsx"
+
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 export interface Props {
   theme: 'ligth' | 'dark'
   mainText: string
   descriptionText: string;
-  buttonCTA1: string;
-  buttonCTA2: string;
-  blackBlock: HTML;
-  greenBlock: string;
+  buttonCta1: {
+    buttonName: string
+    href: string
+  }
+  buttonCta2: {
+    buttonName: string
+    href: string
+  }
+
+  miniBanner: bannerProps
 }
 
-export default function MainBanner({theme, mainText, descriptionText, buttonCTA1, buttonCTA2, blackBlock, greenBlock }: Props) {
+export interface bannerProps{
+  imgLogoSrc: LiveImage,
+  imgLogoAlt: string,
+  bannerTopImage: LiveImage,
+  altBannerTopImage: string,
+  bannerBottomImage: LiveImage,
+  altBannerBottomImage: string,
+}
+
+export default function MainBanner({theme, mainText, descriptionText, buttonCta1, buttonCta2, miniBanner}: Props) {
+
+  const themeIsLigth = theme === 'ligth' ? true : false
+
   return (
     <div class="flex flex-col bg-[#F3FFF9] mb-10 md:mb-0">
       <div class="max-w-screen-2xl m-auto">
-        <div class="px-4 pt-[7rem] md:px-[2rem] md:pt-[11rem]">
+        <div class="px-8 pt-[8rem] md:px-[2rem] md:pt-[11rem]">
           <h1 class="flex flex-row justify-start items-end font-sans font-bold not-italic text-dark-green text-[56px] leading-[3rem] md:( flex-row text-[92px] leading-[6rem] items-start )">
               <span class="w-[100%] lg:(w-[70%])">{mainText}</span>
-              <span class="overflow-y-hidden  -left-[40px] w-[21%] h-[80px] flex justify-start items-center relative text-center lg:(left-0 w-[20%])">
+              <span class="overflow-y-hidden  -left-[40px] top-[20px] w-[21%] h-[80px] flex justify-start items-center relative text-center lg:(left-0 top-0 w-[20%])">
                 <span style="color: #2FD180" class="absolute animate-switcherText10">10%</span>
                 <span style="color: #2FD180" class="absolute animate-switcherText20">20%</span>
                 <span style="color: #2FD180" class="absolute animate-switcherText30">30%</span>
@@ -29,87 +49,41 @@ export default function MainBanner({theme, mainText, descriptionText, buttonCTA1
               </span>
         </h1>
         </div>
-        <div class="flex px-8">
-        <div class="flex flex-col items-center gap-[2rem]">
-          <p class="font-sans font-normal not-italic text-frame-515-rgba text-[32px] leading-[38px]">
+        <div class="flex p-8 flex-col gap-12 justify-center items-center lg:(flex-row)">
+        <div class="flex flex-col gap-[2rem] lg:(w-1/2 items-center)">
+          <p class="font-sans font-normal not-italic text-frame-515-rgba text-[32px] leading-[2rem]">
           {descriptionText}
           </p>
-          
-          <div class="w-full bg-[#1F261F] rounded ">
-            <a href="#delight" class="flex bg-[#1F261F] h-16 justify-start pl-8 items-center text-[#F3FFF9] rounded font-inter font-normal not-italic text-[20px] hover:bg-mytheme-10">
-              {buttonCTA1}
-            </a>
-          </div>
-          <div class="w-full bg-white rounded border-1 border-dark-green">
-            <a href="#delight" class="flex bg-white h-16 justify-center items-center text-dark-green rounded font-inter font-normal not-italic text-[20px] hover:bg-mytheme-10">
-              {buttonCTA2}
-            </a>
+          <div id="buttons-wrapper" class="w-full flex flex-col gap-4">
+            <button class="w-full group flex lg:justify-start justify-center items-center gap-[10px] rounded p-[14px] border-1 border-dark-green bg-dark-green">
+              <a class={"text-[16px] font-medium" + " " + themeIsLigth ? "text-white":"text-dark-green"} href={buttonCta1?.href}>{buttonCta1?.buttonName}</a>
+              <Icon class="hidden transition lg:group-hover:block" id={ themeIsLigth ? "WhiteArrow" : "GreenArrow" } width={15} height={15} strokeWidth={"1"}/>
+            </button>
+            <button class="w-full group flex lg:justify-start justify-center items-center gap-[10px] rounded p-[14px] border-1 border-dark-green">
+              <a class={"text-[16px] font-medium" + " " + themeIsLigth ? "text-dark-green" : "text-white"} href={buttonCta2?.href}>{buttonCta2?.buttonName}</a>
+              <Icon class="hidden transition lg:group-hover:block" id="GreenArrow" width={15} height={15} strokeWidth={"1"} />
+            </button>
           </div>
         </div>
         
-        <div class="relative flex-none md:w-[30rem] grid bg-[#1F261F] rounded-r-full h-[19rem] mb-4 md:mb-0 mr-6 md:ml-[2rem] md:rounded-l-full overflow-hidden group">
-              <QuillText class="pl-7 pr-16 md:pl-[5rem] pt-[3rem] md:w-full font-inter not-italic font-medium text-[22px] leading-[26.5px] text-white md:text-[32px] md:leading-[38.5px]" html={blackBlock}/>
-              <div class="translate-y-[-105px] md:translate-y-[-110px] translate-x-[10px]">
-                <img src="/mobile_group_1/Rectangle 768.png" alt="" class="absolute right-[230px] top-[140px] z-40 group-hover:translate-x-[20px] group-hover:translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 755.png" alt="" class="absolute right-[165px] top-[155px] z-20 group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 766.png" alt="" class="absolute right-[170px] top-[120px] z-20 group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Ellipse 90.png" alt="" class="absolute z-10 right-[200px] top-[120px] group-hover:-translate-y-[20px] group-hover:rotate-45 transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 765.png" alt="" class="absolute right-[165px] top-[125px] z-10 group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Ellipse 89.png" alt="" class="absolute right-[75px] top-[50px] z-20 group-hover:translate-x-[20px] group-hover:-translate-y-[25px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Ellipse 88.png" alt="" class="absolute right-[75px] top-[75px] z-10 group-hover:translate-x-[20px] group-hover:-translate-y-[25px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 763.png" alt="" class="absolute z-10 right-[70px] top-[70px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 761.png" alt="" class="absolute z-10 right-[135px] top-[140px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 760.png" alt="" class="absolute z-10 right-[90px] top-[127px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 764.png" alt="" class="absolute z-10 right-[35px] top-[80px] group-hover:translate-x-[20px] group-hover:-translate-y-[0px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 762.png" alt="" class="absolute z-20 right-[40px] top-[95px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 759.png" alt="" class="absolute z-10 right-[45px] top-[115px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <img src="/mobile_group_1/Rectangle 754.png" alt="" class="absolute right-0 top-[50px] group-hover:-translate-y-[20px] transition ease-in-out duration-700"/>
-                <svg width="260" height="83" viewBox="0 0 260 83" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute right-[5px] top-[100px] z-30 group-hover:translate-y-[20px] transition ease-in-out duration-700" style="filter: blur(2px)">
-                  <g filter="url(#filter0_b_650_44)">
-                    <path d="M0.756064 52.9134C-0.404313 50.6434 0.826482 47.8817 3.29034 47.227L177.88 0.829077C179.701 0.3453 181.612 1.19709 182.469 2.87425L258.744 152.087C259.904 154.357 258.674 157.118 256.21 157.773L81.6197 204.171C79.7993 204.655 77.8881 203.803 77.0307 202.126L0.756064 52.9134Z" fill="#294729" fill-opacity="0.40"/>
-                    <path d="M3.41876 47.7102L178.009 1.3123C179.602 0.888999 181.274 1.63431 182.024 3.10183L258.299 152.314C259.314 154.3 258.237 156.717 256.081 157.29L81.4913 203.688C79.8985 204.111 78.2261 203.366 77.4759 201.898L1.20127 52.6858C0.185937 50.6996 1.26289 48.2831 3.41876 47.7102Z" stroke="white" stroke-opacity="0.97"/>
-                  </g>
-                </svg>
-              </div>
+        <div class="w-screen h-80 lg:(w-[80%] h-[300px]) flex relative -left-[33px] bg-dark-green rounded-r-full">
+            <div>A headless frontend solution, integrated with any platform</div>
+            <div class="w-full relative">
+              <Image 
+                src={miniBanner?.imgLogoSrc}
+                width={450}
+                alt={miniBanner?.imgLogoAlt}
+                title={miniBanner?.imgLogoAlt}
+              />
+              <Image 
+                src={miniBanner?.imgLogoSrc}
+                width={450}
+                alt={miniBanner?.imgLogoAlt}
+                title={miniBanner?.imgLogoAlt}
+              />
+            </div>
           </div>
           </div>
-
-        <div class="md:flex pt-[2.5rem] md:gap-4">
-          
-          <div class="relative flex-auto bg-[#02F67C] rounded-l-full h-[19rem] ml-4 md:mr-[2rem] md:col-span-3 md:rounded-l-none md:rounded-r-full overflow-hidden group">
-              <p class="ml-[5rem] md:ml-[4rem] pt-[3rem] w-[16rem] md:w-[20rem] font-inter not-italic font-medium text-[22px] leading-[26.5px] text-[#1F261F] md:text-[32px] md:leading-[38.5px]">
-                {greenBlock}
-              </p>
-              <div class="py-6 absolute md:w-1/2 md:h-full md:right-0 md:top-0 group">
-                <div class="grid grid-cols-6 gap-2 items-center justify-items-center h-full group">
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_727.png" alt="" class="transform scale-50 rotate-45 translate-x-[45px] group-hover:rotate-0 group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_730.png" alt="" class="transform scale-50 translate-x-[50px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_731.png" alt="" class="transform scale-50 translate-x-[40px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                  <img src="/Rectangle_728.png" alt="" class="transform scale-50 translate-x-[27px] group-hover:scale-100 group-hover:translate-x-0 transition ease-in-out duration-700"/>
-                </div>
-              </div>
-          </div>
-        </div>
       </div>
     </div>
   );
