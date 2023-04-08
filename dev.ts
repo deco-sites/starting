18ca3dde69502109fe1ab11e2d39bd3bb543f5c0
guitavano <tavano62@gmail.com>
2023-04-08 09:04:14 -0300

    update dependencies

--- dev.ts
+++ dev.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env -S deno run -A --watch=static/
 import dev from "$live/dev.ts";
 import liveManifest from "$live/live.gen.ts";
+import liveStdManifest from "deco-sites/std/live.gen.ts";
 
 await dev(import.meta.url, "./main.ts", {
-  imports: [liveManifest],
+  siteId: 530,
+  imports: {
+    "$live": liveManifest,
+    "deco-sites/std": liveStdManifest,
+  },
 });
