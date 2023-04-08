ea849a4e05a4928f1d39a25c20e45cb57c1ce232
jovenan <jovenan2001@gmail.com>
2023-04-08 15:24:59 -0300

    feat: benefits section

---
+++ sections/Benefits.tsx
@@ -0,0 +1,7 @@
+import Benefits, { Props } from '$start/components/ui/Benefits.tsx'
+
+function BenefitsSection(props:Props){
+    return <Benefits {...props}/>
+}
+
+export default BenefitsSection;
