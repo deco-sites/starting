3a60e321d2a282a0bf8b1e35da81fb44c33ba730
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-19 18:10:34 -0300

    Fix blog layout

--- sections/BlogPostHeader.tsx
+++ sections/BlogPostHeader.tsx
@@ -22,16 +22,14 @@ export default function BlogPostHeader(props: Props) {
   const postsList = props.postList;
   const post = postsList.posts.filter((x) => getBlogPath(x.path) === url)[0];
   
-  console.log(1111111111111111, url, postsList);
-  
   if (!post) {
     return <div>Post not found.</div>;
   }
 
   return (
-    <section class="max-w-4xl w-full px-8 md:p-0 mx-auto">
+    <section class="max-w-4xl w-full px-8 md:px-0 mx-auto pt-16">
       <div class="mb-8 sm:mb-11 mt-10 sm:mt-16">
-        <a class="flex items-center cursor-pointer text-subdued" href="/blog">
+        <a class="group flex items-center cursor-pointer text-subdued" href="/pt/blog">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -46,19 +44,19 @@ export default function BlogPostHeader(props: Props) {
               d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
             />
           </svg>
-          <span class="ml-4 hover:underline">
+          <span class="ml-4 group-hover:underline">
             Voltar para o blog
           </span>
         </a>
       </div>
-      <div class="w-full">
-        <h1 class="font-bold text-5xl leading-10 sm:leading-[62px]">
+      <div class="w-full flex flex-col gap-4">
+        <h1 class="text-5xl text-5xl leading-[1.1">
           {post.title}
         </h1>
-        <div class="text-2xl leading-7 my-3 text-secondary-dark">
+        <div class="text-3xl leading-[1.18] text-[#66736C]">
           {post.descr}
         </div>
-        <div class="text-secondary-dark">
+        <div class="text-[#66736C]">
           <span class="mr-5">{post.date}</span>
           <span>{post.author}</span>
         </div>
--- sections/Markdown.tsx
+++ sections/Markdown.tsx
@@ -2,7 +2,7 @@ import Markdown, { Props } from "../components/ui/Markdown.tsx";
 
 export default function MarkdownContainer(props: Props) {
   return (
-    <div class="pt-40 lg:max-w-[894px] mx-auto text-[#66736C] leading-[1.8]">
+    <div class="pt-10 lg:max-w-[894px] mx-auto text-[#66736C] leading-[1.8]">
       <section class="">
         <Markdown {...props}></Markdown>
       </section>
