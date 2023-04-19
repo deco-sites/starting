5d7d86500d1d0063627be3e41e6755e5d9516af3
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-19 14:18:16 -0300

    Fix post list info

--- sections/BlogPostList.tsx
+++ sections/BlogPostList.tsx
@@ -16,6 +16,8 @@ export default function MarkdownContainer(props: Props) {
     postsList.posts.indexOf(post) != 0
   );
 
+  console.log(postsList)
+
   return (
     <section class="pt-40 lg:max-w-[894px] mx-auto">
       <div>
@@ -66,14 +68,14 @@ export default function MarkdownContainer(props: Props) {
               />
               <div class="mt-6 flex flex-col gap-2">
                 <h1 class="text-3xl leading-[1.18]">
-                  {highlightPost.title}
+                  {post.title}
                 </h1>
                 <div class="text-xl leading-[1.5] text-[#66736C]">
-                  {highlightPost.descr}
+                  {post.descr}
                 </div>
                 <div class="leading-[1.8] text-[#66736C]">
-                  <span class="mr-5">{highlightPost.date}</span>
-                  <span>{highlightPost.author}</span>
+                  <span class="mr-5">{post.date}</span>
+                  <span>{post.author}</span>
                 </div>
               </div>
             </div>
