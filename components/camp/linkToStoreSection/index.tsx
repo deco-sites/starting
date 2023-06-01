c280aff63721dcd1179d213d9163c95749fffe8e
wellapos <wellington258@live.com>
2023-06-01 18:44:32 -0300

    fix: adjusted margin on camp component

--- components/camp/linkToStoreSection/index.tsx
+++ components/camp/linkToStoreSection/index.tsx
@@ -7,9 +7,9 @@ export interface Props {
 
 export default function LinkToStoreSection(props: Props) {
   return (
-    <section class="flex flex-col justify-center bg-contact-us-bg-gradient py-[100px]">
+    <section class="flex flex-col justify-center bg-contact-us-bg-gradient py-[12px] md:py-[32px]">
       <div
-        class="font-inter font-regular text-[32px] text-dark-green px-10 py-12 text-center md:text-5xl md:leading-[53px]"
+        class="font-inter font-regular text-[32px] text-dark-green px-10 py-16 text-center md:text-5xl md:leading-[53px]"
         dangerouslySetInnerHTML={{ __html: props.title }}
       />
     </section>
