793ab7d498d34c0bcaa748d12126fe70a674b5c0
gimenes <tlgimenes@gmail.com>
2023-07-11 13:45:04 -0300

    upgrade deps and use std

--- components/utils/markdown.ts
+++ components/utils/markdown.ts
@@ -6,4 +6,4 @@ import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-diff.js?no-check";
 
-export { extract as frontMatter } from "std/encoding/front_matter.ts";
+export { extract as frontMatter } from "std/front_matter/any.ts";
--- main.ts
+++ main.ts
@@ -3,16 +3,14 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="esnext" />
 
-import manifest from "deco-sites/starting/live.gen.ts";
 import { start } from "$fresh/server.ts";
-import prefetchPlugin from "prefetch";
-import partytownPlugin from "partytown/mod.ts";
 import { $live } from "$live/mod.ts";
+import manifest from "deco-sites/starting/live.gen.ts";
+import partytownPlugin from "partytown/mod.ts";
 import site from "./site.json" assert { type: "json" };
 
 await start($live(manifest, site), {
   plugins: [
-    prefetchPlugin(),
     partytownPlugin(),
   ],
 });
