8f36c5e8f9903a6c66de3e64859828452dd568b9
Maria Luiza Aires <84691125+marialuizaaires@users.noreply.github.com>
2023-08-16 14:53:40 -0300

    Luiza/pricingPage (#158)
    * add CTA button
    
    * remove comments
    
    * fix component for mobile design
    
    * fix texts sizes on mobile
    
    * add autogenerated files
    
    * fix block alignment
    
    * add new variant
    
    * remove Button function and import Button component
    
    * add autogenerated file
    
    * fix block padding
    
    * add autogenerated files
    
    ---------
    
    Co-authored-by: Maria Luiza Aires <marialuizaaires@cooper.local>

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -19,6 +19,8 @@ const variants = {
     "h-[36px] px-3 rounded bg-interactive-inverse font-button text-button border-transparent hover:bg-hover active:border-interactive disabled:border-transparent disabled:text-subdued focus:outline-none",
   icon:
     "h-[36px] px-2 rounded-full bg-transparent text-default border-transparent hover:bg-hover active:border-interactive disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
+  inverse:
+    "block w-full h-full text-center font-semibold rounded-[48px] py-3 px-[22px] bg-highlight border-2 border-highlight duration-500 hover:bg-transparent hover:text-highlight",
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(({
