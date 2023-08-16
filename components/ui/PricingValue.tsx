fa6fac5345cfb4744b8affd20aad84f161cc1632
Maria Luiza Aires <marialuizaaires@cooper.local>
2023-08-16 12:48:55 -0300

    fix texts sizes on mobile

--- components/ui/PricingValue.tsx
+++ components/ui/PricingValue.tsx
@@ -24,18 +24,18 @@ export default function PricingValue(
   { price, text, subtext, button }: Props,
 ) {
   return (
-    <section class="flex gap-12 py-12 bg-dark-green justify-center">
+    <section class="flex gap-12 md:py-12 bg-dark-green justify-center">
       <div class="flex flex-col md:flex-row max-w-[1024px] gap-12">
         <div class="md:flex gap-16 px-4 md:px-0 items-center">
-          <div class="flex flex-col gap-4">
-            <h2 class="text-[42px] leading-[1.1] md:text-3xl text-almost-white font-semibold">
+          <div class="flex flex-col gap-4 px-8 md:px-0">
+            <h2 class="text-4xl md:text-[42px] leading-[1.1] md:text-3xl text-almost-white font-semibold">
               <span class="text-highlight text-6xl">{price}</span> {text}
             </h2>
             <p class="text-[13px] text-almost-white">{subtext}</p>
           </div>
         </div>
-        <div>
-          <div class="p-8 rounded-[8px]">
+        <div class="flex md:block justify-center">
+          <div class="w-4/5 md:w-full py-4 mx-auto items-center md:py-8 rounded-[8px]">
             <Button link={button.link} text={button.text} />
           </div>
         </div>
