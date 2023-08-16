49171659ff2d83f59889cb3db07aaf3e34d26c1a
Maria Luiza Aires <84691125+marialuizaaires@users.noreply.github.com>
2023-08-16 11:05:19 -0300

    Luiza/pricing (#155)
    * add PricingValue component
    
    * format document
    
    * fix section padding
    
    * fix section padding
    
    ---------
    
    Co-authored-by: Maria Luiza Aires <marialuizaaires@cooper.local>

---
+++ sections/PricingValue.tsx
@@ -0,0 +1,9 @@
+import PricingValue, {
+  Props,
+} from "deco-sites/starting/components/ui/PricingValue.tsx";
+
+function PricingValueSection(props: Props) {
+  return <PricingValue {...props} />;
+}
+
+export default PricingValueSection;
