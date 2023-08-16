fdfb9819f06ab00a429c55ed7522105578055dbf
Maria Luiza Aires <marialuizaaires@cooper.local>
2023-08-16 12:35:22 -0300

    remove comments

--- components/ui/PricingValue.tsx
+++ components/ui/PricingValue.tsx
@@ -43,17 +43,3 @@ export default function PricingValue(
     </section>
   );
 }
-
-// <section class="flex flex-col gap-12 py-12 bg-dark-green">
-//   <div class="md:flex gap-16 max-w-[1024px] w-1/2 mx-auto px-4 md:px-0">
-//     <div class="flex flex-col gap-4 mx-auto">
-//       <h2 class="text-[42px] leading-[1.1] md:text-3xl text-almost-white font-semibold">
-//         <span class="text-highlight text-6xl">{price}</span> {text}
-//       </h2>
-//       <p class="text-[13px] text-almost-white">{subtext}</p>
-//     </div>
-//     <div class="w-1/2">
-//       <Button link={button.link} text={button.text} />
-//     </div>
-//   </div>
-// </section>
