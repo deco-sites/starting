9dbed847781a59ccc95929f83e9fd370c21245ac
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-08-29 12:02:48 -0300

    Separate title from section

---
+++ sections/Hub/SectionSubtitle.tsx
@@ -0,0 +1,19 @@
+export interface Props {
+  title?: string;
+}
+
+export default function SectionSubtitle(props: Props) {
+  const {
+    title,
+  } = props;
+
+  return (
+    <div class="lg:container mx-4">
+      <div class="container pt-6">
+          <h2 class="text-[20px] font-semibold leading-8 text-[#1F2937] text-center">
+            {title}
+          </h2>
+      </div>
+    </div>
+  );
+}
