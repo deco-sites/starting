6249d7868ecf9e1fb345d006c46d964b7d615a99
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-09-05 11:48:12 -0300

    feat: add sections OnThisPage and Sidebar

---
+++ sections/Sidebar.tsx
@@ -0,0 +1,2 @@
+export { default } from "deco-sites/starting/components/decohelp/pages/ui/Sidebar/Sidebar.tsx";
+export type { SidebarContent } from "deco-sites/starting/components/decohelp/pages/ui/Sidebar/Sidebar.tsx";
