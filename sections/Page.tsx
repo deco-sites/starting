963db4935d7c23bd22d4d970e523ee0357b9f331
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-09-08 08:56:00 -0300

    refactor(Page.tsx): rename props and fix styling

--- sections/Page.tsx
+++ sections/Page.tsx
@@ -2,9 +2,9 @@ import { Section } from "$live/blocks/section.ts";
 import { useLivePageContext } from "$live/pages/LivePage.tsx";
 
 export interface SectionProps {
-  Sidebar: Section;
-  Page: Section;
-  OnThisPage: Section;
+  SectionSidebar: Section;
+  SectionContent: Section;
+  SectionOnThisPage: Section;
 }
 
 export default function (props: SectionProps) {
@@ -12,11 +12,11 @@ export default function (props: SectionProps) {
 
   return (
     <div
-      class={`grid xl:grid-page-desktop lg:grid-page-tablet grid-page-mobile w-full mx-auto max-w-[1440px] lg:gap-[40px]`}
+      class={`grid xl:grid-page-desktop lg:grid-page-tablet grid-page-mobile w-full mx-auto max-w-[1440px] lg:pt-[100px]`}
     >
-      {renderSection(props.Sidebar, 0)}
-      {renderSection(props.Page, 1)}
-      {renderSection(props.OnThisPage, 2)}
+      {renderSection(props.SectionSidebar, 0)}
+      {renderSection(props.SectionContent, 1)}
+      {renderSection(props.SectionOnThisPage, 2)}
     </div>
   );
 }
