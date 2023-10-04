c3eb20f4918478b5cd8a6cd8a49a78ff036c2d1b
guitavano <tavano62@gmail.com>
2023-10-04 13:01:30 -0300

    add proxy images

--- routes/hackathon4.ts
+++ routes/hackathon4.ts
@@ -1,14 +1,14 @@
 import { Handlers } from "$fresh/server.ts";
-import Proxy from "apps/website/handlers/proxy.ts"
 export const handler: Handlers = {
-    async GET(req) {
-    const response = await fetch("https://hackathon-10-23.deco.site") 
-
+  async GET() {
+    const response = await fetch("https://hackathon-10-23.deco.site");
     const text = await response.text();
 
     const modifiedText = text
-    .replaceAll('href="/', 'href="https://hackathon-10-23.deco.site/')
-    .replaceAll("url(/", "url(https://hackathon-10-23.deco.site/");
+      .replaceAll('href="/', 'href="https://hackathon-10-23.deco.site/')
+      .replaceAll("url(/", "url(https://hackathon-10-23.deco.site/")
+      .replaceAll('"/_frsh/', '"https://hackathon-10-23.deco.site/_frsh/')
+      .replaceAll('https://hackathon-10-23.deco.site/sprites.svg', '/proxy/image?image=https://hackathon-10-23.deco.site/sprites.svg');
 
     return new Response(modifiedText, {
       status: response.status,
