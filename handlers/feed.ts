222367d85f7825fe4f8a0b8c3806d6cb5b9f7200
Héricles Emanuel <hericles.me@icloud.com>
2023-11-16 23:18:39 -0300

    feat: add blog rss feed

---
+++ handlers/feed.ts
@@ -0,0 +1,83 @@
+import { Handler } from "deco/mod.ts";
+import { ConnInfo } from "std/http/server.ts";
+import {
+  getBlogPath,
+  Post,
+  PostList,
+  SupportedLocales,
+} from "deco-sites/starting/components/utils/Blog.ts";
+
+interface Props {
+  title: string;
+  description: string;
+  locale: SupportedLocales;
+  list: PostList;
+}
+
+function xmlEncode(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+function generateItemXML(post: Post, locale: SupportedLocales): string {
+  const {
+    body,
+    date,
+    path,
+    author,
+    authorAvatar = "",
+    authorRole = "",
+    readTime = "",
+    img,
+  } = post;
+
+  return `
+    <item>
+      <title>${xmlEncode(body[locale]?.title ?? "")}</title>
+      <link>https://deco.cx/blog${getBlogPath(path, locale)}</link>
+      <description>${xmlEncode(body[locale]?.descr ?? "")}</description>
+      <pubDate>${date}</pubDate>
+      <author>${xmlEncode(author)}</author>
+      <authorAvatar>${xmlEncode(authorAvatar)}</authorAvatar>
+      <authorRole>${xmlEncode(authorRole)}</authorRole>
+      <readTime>${xmlEncode(readTime)}</readTime>
+      <image>${xmlEncode(img)}</image>
+    </item>
+  `;
+}
+
+export default function BlogRSS({
+  title,
+  description,
+  list,
+  locale = "en",
+}: Props): Handler {
+  return (_req: Request, _ctx: ConnInfo) => {
+    const currentDate = new Date().toUTCString();
+    const itemsXML = list.posts
+      .map((post: Post) => generateItemXML(post, locale))
+      .join("\n");
+
+    const rssHeader = `<?xml version="1.0" encoding="UTF-8" ?>
+      <rss version="2.0">
+        <channel>
+          <title>${title}</title>
+          <link>https://deco.cx/${locale}/blog</link>
+          <description>${description}</description>
+          <language>${locale === "en" ? "en-US" : "pt-BR"}</language>
+          <lastBuildDate>${currentDate}</lastBuildDate>
+          ${itemsXML}
+        </channel>
+      </rss>`;
+
+    return new Response(rssHeader, {
+      headers: {
+        "content-type": "text/xml",
+      },
+    });
+  };
+}
