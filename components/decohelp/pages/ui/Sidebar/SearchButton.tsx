96e43b9912366c97ad360239fbdbb9949c004508
Nathan Luiz <nathanluiz33@gmail.com>
2023-11-29 00:19:53 -0300

    adding algolia search button

---
+++ components/decohelp/pages/ui/Sidebar/SearchButton.tsx
@@ -0,0 +1,44 @@
+import { Head } from "$fresh/runtime.ts";
+import { useEffect, useRef } from "preact/hooks";
+import docsearch from "https://esm.sh/@docsearch/js@3.5.2?target=es2020";
+
+// Copied from algolia source code
+type DocSearchProps = {
+  appId: string;
+  apiKey: string;
+  indexName: string;
+  container: HTMLElement | string;
+};
+
+export default function SearchButton(
+  props: { docsearch?: (args: DocSearchProps) => void; },
+) {
+  const ref = useRef<HTMLDivElement>(null);
+  const devMode = window.location.href && window.location.href.includes("__d");
+  useEffect(() => {
+    if (ref.current) {
+      props.docsearch || docsearch({
+        appId: "RBDERIH9EY",
+        apiKey: "1bce943613df73233ccfca9f733b2a47",
+        indexName: "deco",
+        container: ref.current,
+        debug: devMode,
+      });
+    }
+  }, [ref.current]);
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://RBDERIH9EY-dsn.algolia.net" crossorigin />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
+      </Head>
+      <div
+        title="Search Button"
+        class={"h-9 mb-6 " + (props.class ?? "")}
+        ref={ref}
+      >
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/@docsearch/js@3"></script>
+    </>
+  );
+}
