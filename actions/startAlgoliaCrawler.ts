379c9c017e9ae74351dc83928f076c4096cf957a
Nathan Luiz <nathanluiz33@gmail.com>
2023-12-05 12:28:16 -0300

    adding action

---
+++ actions/startAlgoliaCrawler.ts
@@ -0,0 +1,32 @@
+const action = async (): Promise<void> => {
+  const CRAWLER_ID = "2a9862c7-25b6-4e72-a194-7152d768959c";
+  const CRAWLER_API_KEY = "dfecac9615010e34398c83b859e629f1";
+
+  const url =
+    `https://crawler.algolia.com/api/1/crawlers/${CRAWLER_ID}/reindex`;
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Basic " +
+      Buffer.from(`${CRAWLER_ID}:${CRAWLER_API_KEY}`).toString("base64"),
+  };
+
+  await fetch(url, {
+    method: "POST",
+    headers: headers,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+export default action;
