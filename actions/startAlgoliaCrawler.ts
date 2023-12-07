beccb5dd610c69a97efbc4bc414972181cb1346a
Nathan Luiz <nathanluiz33@gmail.com>
2023-12-07 10:49:46 -0300

    removendo comentarios

--- actions/startAlgoliaCrawler.ts
+++ actions/startAlgoliaCrawler.ts
@@ -14,10 +14,6 @@ export interface Props {
 };
 
 const action = async ({crawlerUserId, crawlerApiKey, crawlerId} : Props): Promise<void> => {
-  // const crawlerUserId = "2a9862c7-25b6-4e72-a194-7152d768959c";
-  // const crawlerApiKey = "dfecac9615010e34398c83b859e629f1";
-  // const crawlerId = "8c6a627b-685a-45e6-8674-401b2d960f23";
-
   const url =
     `https://crawler.algolia.com/api/1/crawlers/${crawlerId}/reindex`;
   const headers = {
