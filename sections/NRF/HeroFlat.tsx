4a11c66cb85a9bed82a0640e9e660dc2bdf26772
Héricles <hericles.me@icloud.com>
2024-01-10 10:40:56 -0300

    fix: hero cta position

--- sections/NRF/HeroFlat.tsx
+++ sections/NRF/HeroFlat.tsx
@@ -70,10 +70,16 @@ export default function HeroFlats({
                 <a
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class="group relative relative overflow-hidden rounded-full bg-[#02F67C] px-6 py-2 lg:px-8 lg:py-3 text-black transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-[#02F67C] hover:to-[#06E474] hover:shadow-hero"
+                  class={`group relative relative overflow-hidden rounded-full hover:bg-gradient-to-r px-6 py-2 lg:px-8 lg:py-3 transition-all duration-300 ease-out ${
+                    item.variant === "Reverse"
+                      ? "bg-[#113032] hover:from-[#113032] hover:to-[#0A1F1F] text-white hover:shadow-hero-reverse"
+                      : "bg-[#02F67C] hover:from-[#02F67C] hover:to-[#06E474] text-black hover:shadow-hero"
+                  }`}
                 >
-                  <span class="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white font-medium lg:text-[24px] opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
-                  <span class="relative">{item?.text}</span>
+                  <span class="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
+                  <span class="relative font-medium lg:text-[20px]">
+                    {item?.text}
+                  </span>
                 </a>
               ))}
             </div>
