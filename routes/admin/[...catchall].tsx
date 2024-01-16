b6ba530df7b3c70d8c468043343551688623d2da
Marcos Candeia <marrcooos@gmail.com>
2024-01-16 16:55:00 -0300

    Redirect to admin.deco.cx
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- routes/admin/[...catchall].tsx
+++ routes/admin/[...catchall].tsx
@@ -3,7 +3,7 @@ import type { Handlers } from "$fresh/server.ts";
 export const handler: Handlers = {
   GET(req) {
     const url = new URL(req.url);
-    const urlToRedirect = `https://deco.cx${url.pathname}${url.search}`;
+    const urlToRedirect = `https://admin.deco.cx${url.pathname}${url.search}`;
 
     return Response.redirect(urlToRedirect, 301);
   },
