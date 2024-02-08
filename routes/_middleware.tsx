eada746ecfb05389edb350daf5f0ad485df63bff
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-02-08 10:09:24 -0300

    fix/ send hsts only over https (#252)
    * send hsts header only over https

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -3,10 +3,13 @@ import { FreshContext } from "$fresh/server.ts";
 const HSTS = "max-age=300; includeSubDomains; preload";
 
 export async function handler(
-  _req: Request,
+  req: Request,
   ctx: FreshContext,
 ) {
+  const url = new URL(req.url);
   const resp = await ctx.next();
-  resp.headers.set("Strict-Transport-Security", HSTS);
+  if (url.protocol === "https:") {
+    resp.headers.set("Strict-Transport-Security", HSTS);
+  }
   return resp;
 }
