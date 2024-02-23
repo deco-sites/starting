0f38ea4c091e99573a8343c6440b4e8a98081931
viktormarinho <viktormpcs@gmail.com>
2024-02-23 18:07:30 -0300

    remove theme from hero button

--- components/camp/Button/Button.tsx
+++ components/camp/Button/Button.tsx
@@ -2,22 +2,14 @@ export interface Props {
   label: string;
   details?: string;
   href: string;
-  type?: "ligth" | "dark";
 }
 
-const THEME = {
-  "dark": "bg-primary text-base-200",
-  "ligth": "bg-[#113032] text-primary",
-};
-
 export default function Button(
-  { label, details, href, type = "dark" }: Props,
+  { label, details, href }: Props,
 ) {
   return (
     <a
-      class={` mt-auto rounded-full font-medium text-xl px-6 py-4 ${
-        THEME[type]
-      }`}
+      class={` mt-auto rounded-full font-medium text-xl px-6 py-4 bg-primary text-base-200`}
       href={href}
     >
       {label} <span class="text-sm">{details}</span>
