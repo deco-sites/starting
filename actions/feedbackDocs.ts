93b74f3f950f6fee6ee4f8a1876965d7d98217e6
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2024-02-26 18:38:08 -0300

    feat: add feedback docs (#262)

---
+++ actions/feedbackDocs.ts
@@ -0,0 +1,36 @@
+export interface Props {
+  contents: Array<string | boolean>;
+  url?: string;
+  apiKey?: string;
+}
+
+const action = async (
+  props: Props,
+  _req?: Request,
+): Promise<void> => {
+  const { contents, url, apiKey } = props;
+
+  const payload = {
+    contents,
+    api_key: apiKey || Deno.env.get("GOOGLE_API_KEY"),
+  };
+
+  await fetch(
+    url ||
+      `https://script.google.com/macros/s/${
+        Deno.env.get("GOOGLE_SCRIPT_ID")
+      }/exec`,
+    {
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    },
+  );
+
+  return;
+};
+
+export default action;
---
+++ islands/WasThisPageHelpful.tsx
@@ -0,0 +1,1 @@
+export { default } from "../components/decohelp/pages/ui/WasThisPageHelpful/WasThisPageHelpful.tsx";
