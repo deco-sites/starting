51e1b12ccba02da55cf47674b2e7c560ae1393ff
devartes <anacavalcante731@gmail.com>
2024-02-26 18:16:56 -0300

    refactor: add manifest.gen.ts

--- actions/feedbackDocs.ts
+++ actions/feedbackDocs.ts
@@ -9,16 +9,17 @@ const action = async (
   _req?: Request,
 ): Promise<void> => {
   const { contents, url, apiKey } = props;
- console.log("Sending feedback")
+
   const payload = {
     contents,
     api_key: apiKey || Deno.env.get("GOOGLE_API_KEY"),
   };
-  console.log(payload);
 
-  const response = await fetch(
+  await fetch(
     url ||
-      `https://script.google.com/macros/s/${Deno.env.get("GOOGLE_SCRIPT_ID")}/exec`,
+      `https://script.google.com/macros/s/${
+        Deno.env.get("GOOGLE_SCRIPT_ID")
+      }/exec`,
     {
       headers: {
         accept: "application/json",
@@ -28,10 +29,7 @@ const action = async (
       body: JSON.stringify(payload),
     },
   );
-  console.log(`https://script.google.com/macros/s/${Deno.env.get("GOOGLE_SCRIPT_ID")}/exec`)
-  console.log(await response.text())
-  
- console.log("Feedback sent")
+
   return;
 };
 
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -112,27 +112,29 @@ import * as $$$$$$94 from "./sections/CampMentor.tsx";
 import * as $$$$$$95 from "./sections/Thanks.tsx";
 import * as $$$$$$96 from "./sections/Page.tsx";
 import * as $$$$$$97 from "./sections/Faq.tsx";
-import * as $$$$$$98 from "./sections/DepoimentsCarrossel.tsx";
-import * as $$$$$$99 from "./sections/Links.tsx";
-import * as $$$$$$100 from "./sections/Hub/Advantage.tsx";
-import * as $$$$$$101 from "./sections/Hub/SectionSubtitle.tsx";
-import * as $$$$$$102 from "./sections/Hub/Heading.tsx";
-import * as $$$$$$103 from "./sections/Hub/TemplatesGrid.tsx";
-import * as $$$$$$104 from "./sections/Hub/Agencies.tsx";
-import * as $$$$$$105 from "./sections/Hub/TemplatePreview.tsx";
-import * as $$$$$$106 from "./sections/Hub/SectionTitle.tsx";
-import * as $$$$$$107 from "./sections/Hub/TemplateIframe.tsx";
-import * as $$$$$$108 from "./sections/Hub/Integrations.tsx";
-import * as $$$$$$109 from "./sections/Hub/Creator.tsx";
-import * as $$$$$$110 from "./sections/Delight.tsx";
-import * as $$$$$$111 from "./sections/Benefits.tsx";
-import * as $$$$$$112 from "./sections/About.tsx";
-import * as $$$$$$113 from "./sections/HelpFaq.tsx";
-import * as $$$$$$114 from "./sections/RankingAnalyze.tsx";
-import * as $$$$$$115 from "./sections/RankingHeader.tsx";
-import * as $$$$$$116 from "./sections/Carousel.tsx";
+import * as $$$$$$98 from "./sections/WasThisPageHelpful.tsx";
+import * as $$$$$$99 from "./sections/DepoimentsCarrossel.tsx";
+import * as $$$$$$100 from "./sections/Links.tsx";
+import * as $$$$$$101 from "./sections/Hub/Advantage.tsx";
+import * as $$$$$$102 from "./sections/Hub/SectionSubtitle.tsx";
+import * as $$$$$$103 from "./sections/Hub/Heading.tsx";
+import * as $$$$$$104 from "./sections/Hub/TemplatesGrid.tsx";
+import * as $$$$$$105 from "./sections/Hub/Agencies.tsx";
+import * as $$$$$$106 from "./sections/Hub/TemplatePreview.tsx";
+import * as $$$$$$107 from "./sections/Hub/SectionTitle.tsx";
+import * as $$$$$$108 from "./sections/Hub/TemplateIframe.tsx";
+import * as $$$$$$109 from "./sections/Hub/Integrations.tsx";
+import * as $$$$$$110 from "./sections/Hub/Creator.tsx";
+import * as $$$$$$111 from "./sections/Delight.tsx";
+import * as $$$$$$112 from "./sections/Benefits.tsx";
+import * as $$$$$$113 from "./sections/About.tsx";
+import * as $$$$$$114 from "./sections/HelpFaq.tsx";
+import * as $$$$$$115 from "./sections/RankingAnalyze.tsx";
+import * as $$$$$$116 from "./sections/RankingHeader.tsx";
+import * as $$$$$$117 from "./sections/Carousel.tsx";
 import * as $$$$$$$0 from "./matchers/MatchPath.ts";
 import * as $$$$$$$$$0 from "./actions/startAlgoliaCrawler.ts";
+import * as $$$$$$$$$1 from "./actions/feedbackDocs.ts";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -156,8 +158,8 @@ const manifest = {
     "deco-sites/starting/handlers/feed.ts": $$$$0,
   },
   "sections": {
-    "deco-sites/starting/sections/About.tsx": $$$$$$112,
-    "deco-sites/starting/sections/Benefits.tsx": $$$$$$111,
+    "deco-sites/starting/sections/About.tsx": $$$$$$113,
+    "deco-sites/starting/sections/Benefits.tsx": $$$$$$112,
     "deco-sites/starting/sections/BlogHeader.tsx": $$$$$$10,
     "deco-sites/starting/sections/BlogPostHeader.tsx": $$$$$$22,
     "deco-sites/starting/sections/BlogPostList.tsx": $$$$$$17,
@@ -179,7 +181,7 @@ const manifest = {
     "deco-sites/starting/sections/CampMentor.tsx": $$$$$$94,
     "deco-sites/starting/sections/CampRegistration.tsx": $$$$$$29,
     "deco-sites/starting/sections/CampSteps.tsx": $$$$$$24,
-    "deco-sites/starting/sections/Carousel.tsx": $$$$$$116,
+    "deco-sites/starting/sections/Carousel.tsx": $$$$$$117,
     "deco-sites/starting/sections/Case/ArticleBannerCTA.tsx": $$$$$$79,
     "deco-sites/starting/sections/Case/ArticleDivider.tsx": $$$$$$84,
     "deco-sites/starting/sections/Case/ArticleHeader.tsx": $$$$$$80,
@@ -197,8 +199,8 @@ const manifest = {
     "deco-sites/starting/sections/Content.tsx": $$$$$$27,
     "deco-sites/starting/sections/CustomizableContent.tsx": $$$$$$19,
     "deco-sites/starting/sections/DecoCamp/PricingSection.tsx": $$$$$$48,
-    "deco-sites/starting/sections/Delight.tsx": $$$$$$110,
-    "deco-sites/starting/sections/DepoimentsCarrossel.tsx": $$$$$$98,
+    "deco-sites/starting/sections/Delight.tsx": $$$$$$111,
+    "deco-sites/starting/sections/DepoimentsCarrossel.tsx": $$$$$$99,
     "deco-sites/starting/sections/Differentials.tsx": $$$$$$45,
     "deco-sites/starting/sections/Divider.tsx": $$$$$$31,
     "deco-sites/starting/sections/Faq.tsx": $$$$$$97,
@@ -209,24 +211,24 @@ const manifest = {
     "deco-sites/starting/sections/Head.tsx": $$$$$$49,
     "deco-sites/starting/sections/Header.tsx": $$$$$$76,
     "deco-sites/starting/sections/HelpBlogPosts.tsx": $$$$$$28,
-    "deco-sites/starting/sections/HelpFaq.tsx": $$$$$$113,
+    "deco-sites/starting/sections/HelpFaq.tsx": $$$$$$114,
     "deco-sites/starting/sections/HeroPost.tsx": $$$$$$5,
     "deco-sites/starting/sections/Home/Brands.tsx": $$$$$$6,
     "deco-sites/starting/sections/HowCanWeHelp.tsx": $$$$$$14,
-    "deco-sites/starting/sections/Hub/Advantage.tsx": $$$$$$100,
-    "deco-sites/starting/sections/Hub/Agencies.tsx": $$$$$$104,
-    "deco-sites/starting/sections/Hub/Creator.tsx": $$$$$$109,
-    "deco-sites/starting/sections/Hub/Heading.tsx": $$$$$$102,
-    "deco-sites/starting/sections/Hub/Integrations.tsx": $$$$$$108,
-    "deco-sites/starting/sections/Hub/SectionSubtitle.tsx": $$$$$$101,
-    "deco-sites/starting/sections/Hub/SectionTitle.tsx": $$$$$$106,
-    "deco-sites/starting/sections/Hub/TemplateIframe.tsx": $$$$$$107,
-    "deco-sites/starting/sections/Hub/TemplatePreview.tsx": $$$$$$105,
-    "deco-sites/starting/sections/Hub/TemplatesGrid.tsx": $$$$$$103,
+    "deco-sites/starting/sections/Hub/Advantage.tsx": $$$$$$101,
+    "deco-sites/starting/sections/Hub/Agencies.tsx": $$$$$$105,
+    "deco-sites/starting/sections/Hub/Creator.tsx": $$$$$$110,
+    "deco-sites/starting/sections/Hub/Heading.tsx": $$$$$$103,
+    "deco-sites/starting/sections/Hub/Integrations.tsx": $$$$$$109,
+    "deco-sites/starting/sections/Hub/SectionSubtitle.tsx": $$$$$$102,
+    "deco-sites/starting/sections/Hub/SectionTitle.tsx": $$$$$$107,
+    "deco-sites/starting/sections/Hub/TemplateIframe.tsx": $$$$$$108,
+    "deco-sites/starting/sections/Hub/TemplatePreview.tsx": $$$$$$106,
+    "deco-sites/starting/sections/Hub/TemplatesGrid.tsx": $$$$$$104,
     "deco-sites/starting/sections/ImpactCalculator.tsx": $$$$$$21,
     "deco-sites/starting/sections/Integration.tsx": $$$$$$51,
     "deco-sites/starting/sections/JoinOurCommunity.tsx": $$$$$$18,
-    "deco-sites/starting/sections/Links.tsx": $$$$$$99,
+    "deco-sites/starting/sections/Links.tsx": $$$$$$100,
     "deco-sites/starting/sections/Live Projects/Hero.tsx": $$$$$$88,
     "deco-sites/starting/sections/Live Projects/LiveProjects.tsx": $$$$$$89,
     "deco-sites/starting/sections/Live Projects/ProjectsGrid.tsx": $$$$$$90,
@@ -262,8 +264,8 @@ const manifest = {
     "deco-sites/starting/sections/Pricing.tsx": $$$$$$12,
     "deco-sites/starting/sections/PricingValue.tsx": $$$$$$20,
     "deco-sites/starting/sections/QuillText.tsx": $$$$$$4,
-    "deco-sites/starting/sections/RankingAnalyze.tsx": $$$$$$114,
-    "deco-sites/starting/sections/RankingHeader.tsx": $$$$$$115,
+    "deco-sites/starting/sections/RankingAnalyze.tsx": $$$$$$115,
+    "deco-sites/starting/sections/RankingHeader.tsx": $$$$$$116,
     "deco-sites/starting/sections/RankingList.tsx": $$$$$$11,
     "deco-sites/starting/sections/ReCAPTCHA.tsx": $$$$$$7,
     "deco-sites/starting/sections/SectionList.tsx": $$$$$$13,
@@ -271,6 +273,7 @@ const manifest = {
     "deco-sites/starting/sections/StripePricing.tsx": $$$$$$8,
     "deco-sites/starting/sections/Thanks.tsx": $$$$$$95,
     "deco-sites/starting/sections/TripletBanner.tsx": $$$$$$93,
+    "deco-sites/starting/sections/WasThisPageHelpful.tsx": $$$$$$98,
     "deco-sites/starting/sections/Webinar/Content.tsx": $$$$$$16,
     "deco-sites/starting/sections/Webinar/Hero.tsx": $$$$$$15,
   },
@@ -278,6 +281,7 @@ const manifest = {
     "deco-sites/starting/matchers/MatchPath.ts": $$$$$$$0,
   },
   "actions": {
+    "deco-sites/starting/actions/feedbackDocs.ts": $$$$$$$$$1,
     "deco-sites/starting/actions/startAlgoliaCrawler.ts": $$$$$$$$$0,
   },
   "apps": {
