f0e3b37c177bbd1ba0223df9dc1883509c47c789
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-02-27 14:19:12 -0300

    decocamp: add id to prices section (#265)

--- components/camp/Prices/Prices.tsx
+++ components/camp/Prices/Prices.tsx
@@ -13,7 +13,7 @@ export default function Prices({ props }: { props: Props }) {
   const { plans, content } = props;
 
   return (
-    <div class="flex flex-col w-full gap-6 px-4 py-10 lg:py-20 rounded-b-[40px] md:rounded-b-[100px] container">
+    <div id="prices-section" class="flex flex-col w-full gap-6 px-4 py-10 lg:py-20 rounded-b-[40px] md:rounded-b-[100px] container">
       <div class="flex flex-col gap-6 items-center lg:flex-row lg: justify-center">
         {plans.map((plan) => <CardPrice props={plan} />)}
       </div>
