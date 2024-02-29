2b88823c336fe0c4dfd995cb71db32772e146d6d
ApMatheus <matheusaparecido101@gmail.com>
2024-02-29 07:04:25 -0300

    fix(Faq) Dropdown icon

--- components/camp/Faq/Dropdown.tsx
+++ components/camp/Faq/Dropdown.tsx
@@ -9,17 +9,16 @@ export interface Props {
 export default function Dropdown({ question, answer, position }: Props) {
   return (
     <details
-      class={`border-b border-[#fff] cursor-pointer ${
-        position == 0 ? "border-t" : ""
-      }`}
+      class={`border-b border-[#fff] cursor-pointer faq ${position == 0 ? "border-t" : ""
+        }`}
     >
       <summary class="py-6 md:py-5 font-medium text-[#fff] text-base leading-6 flex items-center justify-between md:text-xl md:leading-[1.875rem]">
-        <p>{question}</p>
+        <p class="w-[calc(100%-32px)]">{question}</p>
         <Icon
-          class="text-white"
+          class="text-white w-auto h-auto md:min-w-[32px] md:min-h-[32px]"
           id="ChevronDown"
-          width={32}
-          height={32}
+          width={20}
+          height={20}
           strokeWidth={"3"}
         />
       </summary>
