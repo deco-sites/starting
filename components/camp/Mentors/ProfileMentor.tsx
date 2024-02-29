d3ca8b1f26fa1eae3a84e58fa2594cc8f576f7d3
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-02-29 18:19:48 -0300

    Fix/decocamp styles (#279)
    * fix video white borders
    
    * use container class for dndpanel
    
    * fix syllabus border color
    
    * Mentors now have optional title + centers on desktop

--- components/camp/Mentors/ProfileMentor.tsx
+++ components/camp/Mentors/ProfileMentor.tsx
@@ -32,7 +32,7 @@ export default function ProfileMentor({ props }: { props: Props }) {
   const { image, nameMentor, profession, content, contacts } = props;
 
   return (
-    <div class="w-full flex flex-col gap-3 md:w-4/12">
+    <div class="w-full flex flex-col gap-3 md:w-1/3">
       <Image
         src={image.src}
         alt={image.alt}
