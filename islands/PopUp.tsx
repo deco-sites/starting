0a04b0de594fcbd12784a55a015b2e61b545303a
ApMatheus <matheusaparecido101@gmail.com>
2024-02-29 14:42:10 -0300

    fear(PopUp) Hero

---
+++ islands/PopUp.tsx
@@ -0,0 +1,6 @@
+import Component from "$store/components/camp/PopUp/PopUp.tsx"
+import type { Props } from "$store/components/camp/PopUp/PopUp.tsx"
+
+export default function Island(props: Props) {
+    return <Component {...props} />
+}
