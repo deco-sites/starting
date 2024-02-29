5d96a9c49fab0817b7a9c91de33e2fd2c4790dcf
Augusto Barbosa <augustocbarbosa@gmail.com>
2024-02-29 18:05:46 -0300

    Remove vimeo bg

--- components/camp/Hero/Hero.tsx
+++ components/camp/Hero/Hero.tsx
@@ -89,7 +89,7 @@ export default function Hero({ props }: { props: Props }) {
    height: calc(16 / 9 * 13vw);
   }">
             <iframe
-              src={`https://player.vimeo.com/video/${idVimeo}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
+              src={`https://player.vimeo.com/video/${idVimeo}?transparent=0&badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
               frameborder="0"
               allow="autoplay; fullscreen; picture-in-picture"
               style="position:absolute;top:0;left:0;width:100%;height:100%;"
