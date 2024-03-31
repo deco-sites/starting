5241c1077cbb45f6d82ad4028ccc4b67b32af767
Martin Adam <84191864+martinadam75@users.noreply.github.com>
2024-03-31 01:07:21 -0300

    Feat: Live Projects + Customer Stories

--- sections/Live Projects/ProjectsGrid.tsx
+++ sections/Live Projects/ProjectsGrid.tsx
@@ -40,6 +40,7 @@ interface Category {
 }
 
 interface Props {
+  projectsTitle?: string;
   itensPerPage?: number;
   nomeDaCategoriaGeral?: string;
   indexCategories?: Category[];
@@ -54,43 +55,20 @@ function CardText(
 ) {
   return (
     <div
-      class={`w-full px-2 flex flex-col gap-3 items-center ${
+      class={`w-full p-6 flex flex-col gap-6 items-start ${
         alignment === "center" ? "text-center" : "text-left"
       }`}
     >
-      <div className="flex items-center w-full">
-        <div class="flex-auto flex flex-col justify-between pl-1">
-          {label && (
-            <h3 class="font-bold text-[24px] leading-7 tracking-[-0.4px]">
-              {label}
-            </h3>
-          )}
-          {category && (
-            <div class="text-[13px] leading-5 uppercase">
-              {category}
-            </div>
-          )}
-        </div>
-        <div class="flex-none font-semibold text-right p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="21"
-            height="20"
-            viewBox="0 0 21 20"
-            fill="none"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M13.1666 4.16667C12.7064 4.16667 12.3333 3.79357 12.3333 3.33333C12.3333 2.8731 12.7064 2.5 13.1666 2.5H17.3333C17.7935 2.5 18.1666 2.8731 18.1666 3.33333V7.5C18.1666 7.96024 17.7935 8.33333 17.3333 8.33333C16.8731 8.33333 16.5 7.96024 16.5 7.5V5.34518L9.58921 12.2559C9.26378 12.5814 8.73614 12.5814 8.4107 12.2559C8.08527 11.9305 8.08527 11.4028 8.4107 11.0774L15.3214 4.16667H13.1666ZM3.89886 5.73223C4.3677 5.26339 5.00358 5 5.66663 5H9.83329C10.2935 5 10.6666 5.3731 10.6666 5.83333C10.6666 6.29357 10.2935 6.66667 9.83329 6.66667H5.66663C5.44561 6.66667 5.23365 6.75446 5.07737 6.91074C4.92109 7.06702 4.83329 7.27899 4.83329 7.5V15C4.83329 15.221 4.92109 15.433 5.07737 15.5893C5.23365 15.7455 5.44561 15.8333 5.66663 15.8333H13.1666C13.3876 15.8333 13.5996 15.7455 13.7559 15.5893C13.9122 15.433 14 15.221 14 15V10.8333C14 10.3731 14.3731 10 14.8333 10C15.2935 10 15.6666 10.3731 15.6666 10.8333V15C15.6666 15.663 15.4032 16.2989 14.9344 16.7678C14.4656 17.2366 13.8297 17.5 13.1666 17.5H5.66663C5.00358 17.5 4.3677 17.2366 3.89886 16.7678C3.43002 16.2989 3.16663 15.663 3.16663 15V7.5C3.16663 6.83696 3.43002 6.20107 3.89886 5.73223Z"
-              fill="#0D1717"
-            />
-          </svg>
-        </div>
+      <div className="flex items-center w-full gap-6">
+        {label && (
+          <h3 class="font-semibold text-[28px] leading-[110%] tracking-[-0.56px] text-white">
+            {label}
+          </h3>
+        )}
       </div>
-      {description && (
-        <div class="text-[13px] text-neutral leading-5 pl-1">
-          {description}
+      {category && (
+        <div class="text-[13px] leading-5 rounded-lg bg-[#212121] text-white p-2">
+          {category}
         </div>
       )}
     </div>
@@ -100,6 +78,7 @@ function CardText(
 function TemplatesGrid(props: Props) {
   const id = `category-cards-${useId()}`;
   const {
+    projectsTitle = "Explore deco's Live Projects",
     itensPerPage = 6,
     indexCategories = [
       {
@@ -200,10 +179,13 @@ function TemplatesGrid(props: Props) {
   const itensPaginaAtual = itensParaExibir.slice(primeiroItem, ultimoItem);
 
   return (
-    <div className="flex flex-col items-center mx-8 pb-10 lg:mx-auto lg:px-14 lg:pb-16 lg:px-16 gap-y-10 lg:pb-20 lg:max-w-[1440px] xl:px-16">
+    <div className="flex flex-col items-center mx-8 pb-10 lg:mx-auto lg:py-20 lg:px-16 gap-y-11 lg:max-w-[1440px] bg-[#010101]">
+      <h2 class="text-white w-full text-left font-albert-sans text-[3rem] font-semibold leading-[120%]">
+        {projectsTitle}
+      </h2>
       <div
         id="index"
-        className="flex flex-start gap-x-12 h-[36px] w-full lg:px-4"
+        className="flex flex-start gap-x-12 h-[36px] w-full"
       >
         <select
           className="p-2 cursor-pointer w-full border-[1px] border-[#C9CFCF] rounded lg:hidden"
@@ -222,12 +204,12 @@ function TemplatesGrid(props: Props) {
             </option>
           ))}
         </select>
-        <div className="hidden lg:flex flex-start border-b-2 gap-x-8 h-[32px] w-full">
+        <div className="hidden lg:flex flex-start border-transparent border-b-2 gap-x-8 h-[32px] w-full">
           <div
             key={{ ...props }.nomeDaCategoriaGeral || "Todos"}
             className={`cursor-pointer relative top-[2px] ${
               (categoriaSelecionada === { ...props }.nomeDaCategoriaGeral)
-                ? "font-bold border-b-2 border-black"
+                ? "text-[#02F67C] font-bold border-b-2 border-[#02F67C]"
                 : ""
             }`}
             onClick={() =>
@@ -242,7 +224,7 @@ function TemplatesGrid(props: Props) {
               key={index}
               className={`cursor-pointer relative top-[2px] ${
                 (categoriaSelecionada === category.label)
-                  ? "font-bold border-b-2 border-black"
+                  ? "text-[#02F67C] font-bold border-b-2 border-[#02F67C]"
                   : ""
               }`}
               onClick={() => handleChangeCategoria(category.label)}
@@ -256,7 +238,7 @@ function TemplatesGrid(props: Props) {
         id={id}
         className="sm:container md:mx-0 flex flex-col text-base-content lg:grid-cols-4 relative"
       >
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-6 md:p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-11">
           {itensPaginaAtual.map(
             ({ label, description, link, image, category }, index) => {
               let categoryInfo;
@@ -276,7 +258,7 @@ function TemplatesGrid(props: Props) {
               return (
                 <a
                   href={`${link}`}
-                  className="flex flex-col gap-4 border border-[#C9CFCF] rounded-lg bg-white overflow-hidden pb-3 md:hover:scale-105 duration-200"
+                  className="flex flex-col border border-[#FFFFFF26] rounded-lg overflow-hidden md:hover:scale-105 duration-200 bg-[#010101]"
                   key={index}
                 >
                   {image && (
@@ -310,15 +292,15 @@ function TemplatesGrid(props: Props) {
           )}
         </div>
       </div>
-      <div id="pagination" className="flex items-center gap-1">
+      <div id="pagination" className="flex items-center gap-4">
         {Array.from({ length: totalPaginas }).map((_, index) => (
           <button
             key={index}
             onClick={() => handleChangePagina(index + 1)}
-            className={`py-2 px-4 rounded ${
+            className={`py-4 px-6 rounded-full ${
               index + 1 === paginaAtual
-                ? "bg-black text-white"
-                : "bg-transparent text-black"
+                ? "bg-[#02F67C] text-black"
+                : "bg-[#113032] text-white"
             }`}
           >
             {index + 1}
