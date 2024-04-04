1422f8440421b9da5499ef296189b60e3ffe468e
gimenes <tlgimenes@gmail.com>
2024-04-04 14:47:26 -0300

    wip

--- apps/site.ts
+++ apps/site.ts
@@ -1,20 +1,23 @@
 import { App, AppContext as AC } from "$live/mod.ts";
-import std, { Props } from "apps/compat/std/mod.ts";
+import std, { Props as StdProps } from "apps/compat/std/mod.ts";
+import $live, { Props as LiveProps } from "apps/compat/$live/mod.ts";
 
 import manifest, { Manifest } from "../manifest.gen.ts";
 
 type StdApp = ReturnType<typeof std>;
+type LiveApp = ReturnType<typeof $live>;
+
+interface Props extends StdProps, LiveProps {}
+
 export default function Site(
   state: Props,
 ): App<Manifest, Props, [
-  StdApp,
+  StdApp, LiveApp
 ]> {
   return {
     state,
     manifest,
-    dependencies: [
-      std(state),
-    ],
+    dependencies: [std(state), $live(state)],
   };
 }
 
