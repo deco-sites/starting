b07571faec7e54cd995b21a1d30d8aa1f2976b27
viktormarinho <viktormpcs@gmail.com>
2024-04-29 17:05:10 -0300

    update contact section to use hubspot embed form

--- sections/NRF/Contact.tsx
+++ sections/NRF/Contact.tsx
@@ -5,37 +5,17 @@ export interface Props {
   title: string;
   /** @format html */
   text: string;
-  /** @description Thanks page language (default en)*/
-  langThanks?: string;
 
-  formInfor?: {
-    buttonId?: string;
-    formTitle?: string;
-    BusinessCTAName?: string;
-    DevCTAName?: string;
-    ContactTitle?: string;
-    PlaceholderfieldName?: string;
-    PlaceholderfieldEmail?: string;
-    PlaceholderfieldPhone?: string;
-    PlaceholderfieldSocial?: string;
-    PlaceholderFieldExtra?: string;
-    PlaceholderfieldUrlSite?: string;
-    PlaceholderfieldPageviews?: string;
-    submiteName?: string;
-    /** @description set the post url to action submit form business*/
-    urlToActionBusiness: string;
-    /** @description set the post url to action submit form developers*/
-    urlToActionDev: string;
-  };
+  /** 
+   * @format textarea 
+   * */
+  embedScript: string;
 }
-
-const CONTACT_FORM_ID = "contact-form";
 
 export default function ContactUs({
   title,
   text,
-  langThanks = "en",
-  formInfor,
+  embedScript,
 }: Props) {
   return (
     <section class="flex flex-col gap-12 lg:py-[64px] lg:py-28 2xl:py-36 bg-black">
@@ -51,62 +31,13 @@ export default function ContactUs({
         </div>
         <div class="hidden lg:block border-r border-white/[0.1]"></div>
         <div class="w-full relative max-w-md">
-          <form
-            id={CONTACT_FORM_ID}
-            class="flex flex-col justify-center items-center gap-6 placeholder-[#161A16]"
-            method="POST"
-            action="/api/leads"
-          >
-            <input type="hidden" name="lang" value={langThanks} />
-            <input
-              class="w-full h-[51px] border border-white/[.1] rounded placeholder-white bg-white/5 text-white backdrop-blur-xl p-4"
-              type="text"
-              name="firstname"
-              placeholder={formInfor?.PlaceholderfieldName || "Name*"}
-              required
-            />
-            <input
-              class="w-full h-[51px] border border-white/[.1] rounded placeholder-white bg-white/5 text-white backdrop-blur-xl p-4"
-              type="email"
-              name="email"
-              placeholder={formInfor?.PlaceholderfieldEmail || "Work e-mail*"}
-              required
-            />
-            <input
-              class="w-full h-[51px] border border-white/[.1] rounded placeholder-white bg-white/5 text-white backdrop-blur-xl p-4"
-              type="text"
-              name="mobilephone"
-              placeholder={formInfor?.PlaceholderfieldPhone || "Phone*"}
-              required
-            />
-            <input
-              class="w-full h-[51px] border border-white/[.1] rounded placeholder-white bg-white/5 text-white backdrop-blur-xl p-4"
-              type="text"
-              name="linkedin"
-              placeholder={formInfor?.PlaceholderfieldSocial ||
-                "Linkedin (optional)"}
-            />
-            <input
-              class="w-full h-[51px] border border-white/[.1] rounded placeholder-white bg-white/5 text-white backdrop-blur-xl p-4"
-              type="text"
-              name="website"
-              placeholder={formInfor?.PlaceholderfieldUrlSite || "Site URL*"}
-              required
-            />
-            <input
-              id={formInfor?.buttonId}
-              type="submit"
-              value={formInfor?.submiteName || "Get early access"}
-              class={`g-recaptcha w-full cursor-pointer group relative relative overflow-hidden rounded-full hover:bg-gradient-to-r px-6 py-2 lg:px-8 lg:py-3 transition-all duration-300 ease-out bg-[#02F67C] hover:from-[#02F67C] hover:to-[#06E474] text-black hover:shadow-hero font-medium lg:text-[20px]`}
-              data-sitekey={RECAPTCHA_SITE_KEY}
-              data-callback="onSubmit"
-              data-action="submit"
-            >
-            </input>
-          </form>
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+        <script dangerouslySetInnerHTML={{
+            __html: embedScript,
+          }}>
+        </script>
         </div>
       </div>
-      <ReCAPTCHA formId={CONTACT_FORM_ID} />
     </section>
   );
 }
