570936cb2cc7449f4fdd4f5ac3d86c4ecfccae35
Martin Adam <martin.viana99@gmail.com>
2024-05-10 11:44:19 -0300

    Markdown changes + bg contrast

--- components/ui/Markdown.tsx
+++ components/ui/Markdown.tsx
@@ -55,6 +55,7 @@ export default function Markdown({ post }: Props) {
   body = body.replace(/<hr/g, '<hr class="border border-[#FFFFFF26]"');
   body = body.replace(/<code/g, '<code class="text-[#F9FAFB] bg-[#FFFFFF0D] backdrop-filter backdrop-blur-24 py-0.5"');
   body = body.replace(/<pre/g, '<pre class="border-2 border-[#FFFFFF26] bg-[#FFFFFF0D] rounded backdrop-filter backdrop-blur-24 p-4"');
+  body = body.replace(/<ul/g, '<ul class="marker:text-green-500"');
 
   return (
     <div
