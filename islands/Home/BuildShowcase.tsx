507ce4239b47d666c474e5020cb052f3032c8bc1
Martin Adam <martin.viana99@gmail.com>
2024-05-29 08:02:17 -0300

    Feat: BuildShowcase Mobile

--- islands/Home/BuildShowcase.tsx
+++ islands/Home/BuildShowcase.tsx
@@ -1,42 +1,56 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
-import { ShowcaseEditorTabbed, Tab } from "site/islands/NRF/ShowcaseEditorTabbed.tsx";
+import {
+  ShowcaseEditorTabbed,
+  Tab,
+} from "site/islands/NRF/ShowcaseEditorTabbed.tsx";
+import { ShowcaseEditorAccordion } from "../NRF/ShowcaseEditorAccordion.tsx";
 import { Head } from "$fresh/runtime.ts";
-
-interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  variant: "Normal" | "Reverse" | "Product Hunt";
-}
+import { AppContext } from "site/apps/site.ts";
 
 export interface Props {
   /**
-     * @format rich-text
-     * @default Click here to tweak this text however you want.
-     */   
+   * @format rich-text
+   * @default Click here to tweak this text however you want.
+   */
   title?: string;
   /**
    * @format html
    */
   subtitle?: string;
   tabs?: Tab[];
-  position?: 'left' | 'right'
-  hideEditor?: boolean;
+  position?: "left" | "right";
 }
+
+export const loader = async (
+  props: Props,
+  req: Request,
+  ctx: AppContext,
+) => {
+  const device = ctx.device;
+
+  return {
+    ...props,
+    isMobile: device,
+  };
+};
 
 export default function BuildShowCase({
   title,
   subtitle,
   tabs,
-  hideEditor,
-  position
-}: Props) {
+  position,
+  isMobile,
+}: Omit<Props, "isMobile"> & {
+  title?: string;
+  subtitle?: string;
+  tabs?: Tab[];
+  position?: "left" | "right";
+  isMobile?: string;
+}) {
   return (
     <div
       id="hero"
-      class={`relative bg-black space-y-16 lg:space-y-20 ${
-        hideEditor ? "" : "min-h-screen"
-      }`}
+      class={`relative bg-black py-20 space-y-16 lg:space-y-20 px-6`}
     >
       <Head>
         <script>
@@ -50,13 +64,13 @@ export default function BuildShowCase({
           });`}
         </script>
       </Head>
-      <div class="max-w-fit mx-auto flex flex-col items-center gap-16 lg:gap-20">
-        <div class="flex flex-col items-center gap-4 mt-36 lg:mt-48">
+      <div class="mx-auto flex flex-col items-center gap-16 lg:gap-20 border-t border-[#16E47D]">
+        <div class="flex flex-col items-center gap-4 border-t border-[#16E47D]">
           <div class="flex flex-col items-center gap-6 lg:gap-12 z-10">
             <div
-              class="mx-6 lg:mx-0 inline-block text-[32px] lg:text-[48px] text-center leading-[115%] lg:tracking-[-1.44px] font-medium text-white max-w-lg lg:max-w-none"
+              class="mx-6 lg:mx-0 inline-block text-[32px] lg:text-[48px] text-center leading-[115%] lg:tracking-[-1.44px] font-medium text-white max-w- lg:max-w-none"
               dangerouslySetInnerHTML={{
-                __html: title ?? '',
+                __html: title ?? "",
               }}
             >
             </div>
@@ -69,32 +83,14 @@ export default function BuildShowCase({
               >
               </div>
             )}
-            {/* {cta && (
-              <div class="flex flex-col items-center lg:items-start lg:flex-row gap-4">
-                {(demo?.visible) && (
-                  <label
-                    for="demo-modal"
-                    id={demo?.id}
-                    class={`group relative relative overflow-hidden rounded-full hover:bg-gradient-to-r px-6 py-2 lg:px-8 lg:py-3 transition-all duration-300 ease-out bg-[#DA8FFF] hover:from-[#DA8FFF] hover:to-[#DA8FFF] text-[113032] hover:shadow-watch-our-demo`}
-                  >
-                    <span class="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40">
-                    </span>
-                    <span class="text-black relative font-medium lg:text-[20px]">
-                      {demo?.title || "Watch our demo"}
-                    </span>
-                  </label>
-                )}
-              </div>
-            )} */}
           </div>
         </div>
       </div>
-      {!hideEditor && (
-        <div class="mx-auto flex flex-col items-center">
-          <ShowcaseEditorTabbed tabs={tabs} position={position}/>
-        </div>
-      )}
-
+      <div class="mx-auto flex flex-col items-center">
+        {isMobile == "desktop"
+          ? <ShowcaseEditorTabbed tabs={tabs} position={position} />
+          : <ShowcaseEditorAccordion tabs={tabs} position={position} />}
+      </div>
     </div>
   );
 }
--- islands/NRF/ShowcaseEditorTabbed.tsx
+++ islands/NRF/ShowcaseEditorTabbed.tsx
@@ -29,7 +29,7 @@ export const ShowcaseEditorTabbed = ({ tabs, position = "left" }: Props) => {
       const animationWidth = animate(
         `#tab-${selectedTab.value}`,
         { width: "100%" },
-        { duration: 10, easing: "linear" },
+        { duration: 5, easing: "linear" },
       );
 
       const tabElements = document.querySelectorAll("[id^='tab-']");
@@ -50,71 +50,84 @@ export const ShowcaseEditorTabbed = ({ tabs, position = "left" }: Props) => {
   }, [tabs, selectedTab.value]);
 
   return (
-    <div
-      class={`flex ${
-        position === "left" ? "" : "flex-row-reverse "
-      }lg:w-full lg:max-w-[1440px] lg:px-[120px] rounded-lg lg:rounded-3xl mx-2 gap-8 lg:mx-0 z-40 backdrop-blur-xl`}
-    >
-      <div class="flex flex-col w-[48%] justify-center h-full gap-x-8 gap-2 text-white flex-wrap">
-        {tabs &&
-          tabs.map((tab, index) => (
-            <div
-              class={`flex flex-col p-6 rounded-[16px] justify-center items-start ${
-                index === selectedTab.value
-                  ? "bg-[#0D1717]"
-                  : "text-zinc-600 border-transparent"
-              }`}
-            >
-              <button
-                onClick={() => {
-                  selectedTab.value = index;
-                }}
-                class={`w-full flex gap-4 text-[12px] items-center lg:text-[18px] leading-[135%] tracking-[-0.36px] pb-4 text-left font-medium border-b duration-200 transition-colors ${
+    <>
+      <div
+        class={`flex flex-col lg:flex-row ${
+          position === "left" ? "" : "flex-row-reverse "
+        }lg:w-full lg:max-w-[1440px] lg:px-[120px] rounded-lg lg:rounded-3xl mx-2 gap-8 lg:mx-0 z-40 backdrop-blur-xl`}
+      >
+        <div class="flex flex-col w-[48%] justify-center h-full gap-x-8 gap-2 text-white flex-wrap">
+          {tabs &&
+            tabs.map((tab, index) => (
+              <div
+                class={`flex flex-col p-6 rounded-[16px] justify-center items-start ${
                   index === selectedTab.value
-                    ? "text-[#02F67C]"
+                    ? "bg-[#0D1717]"
                     : "text-zinc-600 border-transparent"
                 }`}
               >
+                <button
+                  onClick={() => {
+                    selectedTab.value = index;
+                  }}
+                  class={`w-full flex flex-col gap-4 text-[12px] items-center lg:text-[18px] leading-[135%] tracking-[-0.36px] pb-4 text-left font-medium border-b border-[#616B6B] duration-200 transition-colors ${
+                    index === selectedTab.value
+                      ? "text-white"
+                      : "text-zinc-600 border-transparent"
+                  }`}
+                >
+                  <div class="flex gap-4 w-full">
+                    <Image
+                      src={tab.icon}
+                      alt={tab.title}
+                      width={24}
+                      height={24}
+                      preload={index === 0 ? true : false}
+                    />
+                    {tab.title}
+                    {tab.label?.name &&
+                      (
+                        <span
+                          class="px-2 py-1 flex items-center justify-center rounded-[53px] text-[10px] leading-[120%] text-[#0D1717]"
+                          style={{ backgroundColor: tab.label?.color }}
+                        >
+                          {tab.label?.name}
+                        </span>
+                      )}
+                  </div>
+                  <span
+                    class={`font-normal text-[14px] leading-[150%] ${
+                      index === selectedTab.value ? "" : "hidden"
+                    }`}
+                  >
+                    {tab.description}
+                  </span>
+                </button>
+                <div id={`tab-${index}`} class="h-[1px] bg-[#02F67C] w-0"></div>
+              </div>
+            ))}
+        </div>
+        <div>
+          {tabs &&
+            tabs.map((tab, index) => (
+              <div
+                class={`flex justify-center items-center ${
+                  index !== selectedTab.value ? "hidden" : ""
+                }`}
+              >
                 <Image
-                  src={tab.icon}
+                  src={tab.image}
                   alt={tab.title}
-                  width={24}
-                  height={24}
+                  width={1134}
+                  height={746}
                   preload={index === 0 ? true : false}
                 />
-                {tab.title}
-                {tab.label?.name &&
-                  (
-                    <span
-                      class="px-2 py-1 flex items-center justify-center rounded-[53px] text-xs font-semibold leading-[120%] text-[#0D1717]"
-                      style={{ backgroundColor: tab.label?.color }}
-                    >
-                      {tab.label?.name}
-                    </span>
-                  )}
-              </button>
-              <div id={`tab-${index}`} class="h-[1px] bg-[#02F67C] w-0"></div>
-            </div>
-          ))}
+              </div>
+            ))}
+        </div>
       </div>
-      <div>
-        {tabs &&
-          tabs.map((tab, index) => (
-            <div
-              class={`flex justify-center items-center ${
-                index !== selectedTab.value ? "hidden" : ""
-              }`}
-            >
-              <Image
-                src={tab.image}
-                alt={tab.title}
-                width={1134}
-                height={746}
-                preload={index === 0 ? true : false}
-              />
-            </div>
-          ))}
-      </div>
-    </div>
+
+      
+    </>
   );
 };
