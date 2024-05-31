1a0d686bd966eb0d8ed365d26bf84a608a58b9ec
Martin Adam <martin.viana99@gmail.com>
2024-05-31 06:54:58 -0300

    Feat: CarouselLinks

--- components/ui/CarouselLinks.tsx
+++ components/ui/CarouselLinks.tsx
@@ -16,68 +16,105 @@ export interface Post {
 
 export interface Props {
   bottomPadding?: string;
+  /**
+   * @format rich-text
+   * @default Click here to tweak this text however you want.
+   */
+  title?: string;
   cards: Post[];
 }
 
 export default function CarouselLinks({
   bottomPadding,
+  title =
+    "Lorem ipsum dolor sit amet consectetur.<br>Lectus diam pellentesque sit velit.",
   cards,
 }: Props) {
   const id = useId();
 
   return (
     <section
-      class="bg-black"
+      class="flex flex-col relative px-6 py-16 lg:px-16 gap-8 lg:gap-12 max-w-screen-2xl m-auto lg:py-36 z-10"
       style={{ paddingBottom: bottomPadding }}
       id={id}
     >
-      
-      <div class="relative px-6 md:px-[7rem] max-w-screen-2xl m-auto py-28 z-10">
-        <div className="before:absolute before:inset-y-0 before:left-0 before:w-64 before:bg-gradient-to-l before:from-black/0 before:to-[#030806] before:z-20">
+      {title &&
+        (
+          <h2
+            class="font-albert-sans text-lg lg:text-2xl font-medium text-white text-center leading-[115%] tracking-[-0.72px]"
+            dangerouslySetInnerHTML={{
+              __html: title,
+            }}
+          >
+          </h2>
+        )}
+      <div class="relative">
+        <div className="before:absolute before:inset-y-0 before:left-0 before:w-[10%] before:bg-gradient-to-l before:from-black/0 before:to-[#030806] before:z-20">
         </div>
-        <div className="after:absolute after:inset-y-0 after:right-0 after:w-64 after:bg-gradient-to-r after:from-black/0 after:to-[#030806] after:z-20">
+        <div className="after:absolute after:inset-y-0 after:right-0 after:w-[10%] after:bg-gradient-to-r after:from-black/0 after:to-[#030806] after:z-20">
         </div>
-        <SliderFocus
-          class="relative gap-4 carousel carousel-center col-span-full row-start-2 row-end-5 auto-rows-fr hidden-scroll"
-          itemClass="h-full"
-          snap="opacity-50 disabled:opacity-100 focus:outline-none h-full"
-        >
-          {cards.map((card) => {
-            return (
-              <a
-                href={card.href}
-                class="group rounded-[8px] flex w-[320px] h-full"
-              >
-                <article class="flex flex-col gap-6 overflow-hidden backdrop-filter backdrop-blur-22">
-                  <div class="rounded-[8px]">
-                    <Image
-                      src={card.img as string}
-                      fetchPriority={"low"}
-                      class="w-full aspect-[16/9] object-cover"
-                      preload={false}
-                      loading={"lazy"}
-                      width={IMAGE_WIDTH}
-                      height={getAspectRatio(IMAGE_WIDTH, 16 / 9)}
-                    />
-                  </div>
-                  <div class="flex flex-col gap-4 justify-between rounded-[8px] h-[47%]">
-                    <div class="flex flex-col gap-4">
-                      <h5 class="font-medium not-italic text-left text-[14.5px] lg:text-[24px] text-white opacity-[80%]leading-[1.18] group-hover:text-[#4ADE80]">
-                        {card.label}
-                      </h5>
-                        <div class="text-[9.66px] lg:text-[16px] text-[#9CA3AF] leading-[1.5] line-clamp-2 overflow-ellipsis text-left">
-                        {card.description}
+
+        {/* Contêiner para os cards */}
+        <div class="relative overflow-hidden">
+          <div class="flex">
+            <SliderFocus
+              class="flex gap-4 carousel-start"
+              itemClass="h-full"
+              snap="opacity-50 disabled:opacity-100 focus:outline-none h-full"
+            >
+              {cards.map((card, index) => (
+                <a
+                  href={card.href}
+                  class="group block rounded-[8px] w-[71%] sm:w-[193px] sm flex-none lg:w-[320px] min-h-[230px] lg:min-h-[362px] h-full transition-all duration-300 sm:hover:w-[232px] lg:hover:w-[384px]"
+                >
+                  <article class="flex flex-col gap-6 overflow-hidden backdrop-filter backdrop-blur-22 transition-all duration-300">
+                    <div class="rounded-[8px]">
+                      <Image
+                        src={card.img as string}
+                        fetchPriority={"low"}
+                        class="w-full aspect-[16/9] object-cover"
+                        preload={false}
+                        loading={"lazy"}
+                        width={IMAGE_WIDTH}
+                        height={getAspectRatio(IMAGE_WIDTH, 16 / 9)}
+                      />
+                    </div>
+                    <div class="flex flex-col gap-4 justify-between rounded-[8px] h-[47%] group-hover:space-y-0">
+                      <div class="flex flex-col gap-4 overflow-hidden">
+                        <h5 class="w-[384px] flex gap-2 font-medium not-italic text-left text-[14.5px] leading-[115%] tracking-[-0.72px] lg:text-[24px] text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                          {card.label}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                          >
+                            <path
+                              d="M12 6H6C5.46957 6 4.96086 6.21071 4.58579 6.58579C4.21071 6.96086 4 7.46957 4 8V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H16C16.5304 20 17.0391 19.7893 17.4142 19.4142C17.7893 19.0391 18 18.5304 18 18V12M11 13L20 4M20 4H15M20 4V9"
+                              stroke="#616B6B"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </svg>
+                        </h5>
+                        <div class="w-[384px] text-[9.66px] lg:text-[16px] text-[#9CA3AF] leading-[1.5] line-clamp-2 overflow-ellipsis text-left opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                          {card.description}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </article>
-              </a>
-            );
-          })}
-        </SliderFocus>
-        <div class="flex flex-row justify-end gap-2 pt-6 pb-12 md:pt-0 md:pb-0 md:py-0">
+                  </article>
+                </a>
+              ))}
+            </SliderFocus>
+          </div>
+        </div>
+
+        {/* Botões de navegação */}
+        <div class="flex flex-row justify-end">
           <button
-            class="rounded-full p-3 bg-[#02F67C] focus:outline-none flex items-center justify-center border-none border-0 md:absolute md:left-[6rem] md:top-[40%] z-40"
+            class="rounded-full p-3 bg-[#02F67C] focus:outline-none flex items-center justify-center border-none border-0 absolute left-[0.5rem] sm:left-[6rem] top-[18%] z-40"
             data-slide="prev"
             aria-label="Previous item"
           >
@@ -97,7 +134,7 @@ export default function CarouselLinks({
             </svg>
           </button>
           <button
-            class="rounded-full p-3 bg-[#02F67C] focus:outline-none flex items-center justify-center border-none border-0 md:absolute md:top-[40%] right-[6rem] z-40"
+            class="rounded-full p-3 bg-[#02F67C] focus:outline-none flex items-center justify-center border-none border-0 absolute top-[18%] right-[0.5rem] sm:right-[6rem] z-40"
             data-slide="next"
             aria-label="Next item"
           >
