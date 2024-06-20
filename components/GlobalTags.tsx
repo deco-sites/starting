e4e3c426e37f69593ec071b562317c08db18a976
Tiago Gimenes <gimenes@deco.cx>
2024-06-20 18:39:52 +0000

    Release staging environment with 2 changes made by Luciano Júnior, Martin Henrique Viana Adam, Jonatas Venancio, Tiago Gimenes

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -1,10 +1,20 @@
 import { asset, Head } from "$fresh/runtime.ts";
 import Theme from "$store/sections/Theme/Theme.tsx";
 
+const getFontFace = () => `@font-face {
+  font-family: 'argent-pixel';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(${asset('/fonts/ArgentPixelCF-Regular.woff2')}) format('woff2');
+}`
+
 export default function GlobalTags() {
   return (
     <>
       <Head>
+        <style dangerouslySetInnerHTML={{ __html: getFontFace() }} />
+
         <link href={asset("/styles.css")} rel="stylesheet" />
         {/* Plausible Analytics */}
         <script
@@ -45,9 +55,8 @@ export default function GlobalTags() {
                     font-style: swap;
                     font-weight: 400;
                     font-display: display;
-                    src: url(${
-              asset("/fonts/font_albert.woff2")
-            }) format('woff2');
+                    src: url(${asset("/fonts/font_albert.woff2")
+              }) format('woff2');
                     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                 }
                 /* latin */
@@ -56,9 +65,8 @@ export default function GlobalTags() {
                     font-style: swap;
                     font-weight: 500;
                     font-display: display;
-                    src: url(${
-              asset("/fonts/font_albert.woff2")
-            }) format('woff2');
+                    src: url(${asset("/fonts/font_albert.woff2")
+              }) format('woff2');
                     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                 }
                 /* latin */
@@ -67,9 +75,8 @@ export default function GlobalTags() {
                     font-style: swap;
                     font-weight: 600;
                     font-display: display;
-                    src: url(${
-              asset("/fonts/font_albert.woff2")
-            }) format('woff2');
+                    src: url(${asset("/fonts/font_albert.woff2")
+              }) format('woff2');
                     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                 }
                 /* latin */
@@ -78,9 +85,8 @@ export default function GlobalTags() {
                     font-style: swap;
                     font-weight: 700;
                     font-display: display;
-                    src: url(${
-              asset("/fonts/font_albert.woff2")
-            }) format('woff2');
+                    src: url(${asset("/fonts/font_albert.woff2")
+              }) format('woff2');
                     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                 }
 
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,7 @@
 import { PageProps } from "$fresh/server.ts";
 import GlobalTags from "site/components/GlobalTags.tsx";
+
+
 function App({ Component }: PageProps) {
   return (
     <>
