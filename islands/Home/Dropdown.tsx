69b94de2aae56d016aeca85acbcc4204e9383a5e
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-07-05 16:03:23 -0300

    lint fix

--- islands/Home/Dropdown.tsx
+++ islands/Home/Dropdown.tsx
@@ -125,9 +125,7 @@ export function Dropdown({ columns, value, variant = "flat" }: Props) {
                     </p>
                   </div>
                 )}
-                {col.nested?.map((item) => (
-                  <DropdownItem {...item} />
-                ))}
+                {col.nested?.map((item) => <DropdownItem {...item} />)}
               </div>
               {columns.length - 1 > index && (
                 <div class="w-[1px] bg-[#3e4a44] h-[300px]"></div>
