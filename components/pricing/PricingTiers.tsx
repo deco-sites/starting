c6eeff5e9fc5bd413ae8fa985a49f58dbfd4d081
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-07-12 11:46:54 -0300

    Adds toggle to show menu

--- components/pricing/PricingTiers.tsx
+++ components/pricing/PricingTiers.tsx
@@ -24,10 +24,16 @@ export interface PricingTiersProps {
    * @description 15 = 15%
    */
   annualDiscount?: number;
+  showToggleMenu?: boolean;
   notes?: Note[];
 }
 
-function PricingTiers({ notes, tiers, annualDiscount }: PricingTiersProps) {
+function PricingTiers({
+  notes,
+  tiers,
+  annualDiscount,
+  showToggleMenu,
+}: PricingTiersProps) {
   const applyAnnualDiscount = useSignal(false);
 
   function handleUpdateDiscount(value: boolean) {
@@ -37,9 +43,14 @@ function PricingTiers({ notes, tiers, annualDiscount }: PricingTiersProps) {
   return (
     <div class="flex justify-center elative w-full">
       <div class="relative flex flex-col items-center gap-8 z-10 max-w-[1440px] mx-4">
-        <div class="bg-[#070D0D] border border-[#0B1612] p-0.5 rounded-[10px]">
-          <SelectTimePlan updateDiscount={handleUpdateDiscount} annualDiscount={annualDiscount} />
-        </div>
+        {showToggleMenu && (
+          <div class="bg-[#070D0D] border border-[#0B1612] p-0.5 rounded-[10px]">
+            <SelectTimePlan
+              updateDiscount={handleUpdateDiscount}
+              annualDiscount={annualDiscount}
+            />
+          </div>
+        )}
         <div class="grid grid-cols-1 md:grid-cols-2 lg:flex text-white gap-4 w-full">
           {tiers?.map((tier) => (
             <PricingCard
