46dfca9ee9328f06378a4e9c687cfc1352dce5c4
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-07-15 22:26:34 -0300

    Greate Removal sections

--- sections/QuillText.tsx
+++
@@ -1,4 +0,0 @@
-import QuillText from "deco-sites/std/components/QuillText.tsx";
-export * from "deco-sites/std/components/QuillText.tsx";
-
-export default QuillText;
--- sections/WasThisPageHelpful.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from "site/components/decohelp/pages/ui/WasThisPageHelpful/WasThisPageHelpful.tsx";
