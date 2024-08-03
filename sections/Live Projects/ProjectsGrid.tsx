f36d521fb4866c0d05a3ddd6a8f81a5b1af27972
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-08-03 14:49:01 -0300

    fix costumer stories bug

--- sections/Live Projects/ProjectsGrid.tsx
+++ sections/Live Projects/ProjectsGrid.tsx
@@ -61,22 +61,20 @@ function ProjectCard({ link, label, category, image, icon }: TemplateInfo) {
     <a href={`${link}`} target="_blank" className="flex flex-col gap-4">
       {image && (
         <div class="rounded-2xl p-2 border border-[#0B1612] bg-[#070D0D]">
-          <div class="group overflow-y-hidden max-h-[400px] md:max-h-[300px] rounded-lg object-top">
+          <div class="group overflow-y-hidden h-[400px] md:h-[300px] rounded-lg object-top">
             <Image
-              className="group-hover:translate-y-[-8rem] h-full transition duration-[2000ms] ease-in-out object-top object-cover"
+              className="w-full group-hover:translate-y-[-8rem] h-full transition duration-[2000ms] ease-in-out object-top object-cover"
               src={image}
               alt={label}
               width={398}
               loading="lazy"
-              style={{ width: "100%" }}
             />
             <Image
-              className="group-hover:translate-y-[-8rem] h-full transition duration-[2000ms] ease-in-out object-top object-cover"
+              className="w-full group-hover:translate-y-[-8rem] h-full transition duration-[2000ms] ease-in-out object-top object-cover"
               src={image}
               alt={label}
               width={398}
               loading="lazy"
-              style={{ width: "100%" }}
             />
           </div>
         </div>
