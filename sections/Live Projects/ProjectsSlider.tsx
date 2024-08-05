b4b1778021622008e60e88ff256fc1463d03b41d
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-08-05 11:27:02 -0300

    Fix background glow

--- sections/Live Projects/ProjectsSlider.tsx
+++ sections/Live Projects/ProjectsSlider.tsx
@@ -6,13 +6,14 @@ export interface Props {
 
 export default function ProjectsSlider({ projects }: Props) {
   return (
-    <div class="flex justify-center relative z-10">
-      <div class="deco-background"></div>
-      <div
-        class="h-[200vh] z-10 w-[100vw] md:w-[100vw] absolute lg:w-[100vw] top-[-550px] scale-150 lg:scale-100"
-        style="background: radial-gradient(circle, rgba(2,246,124,.35) 0%, rgba(255,255,255,0) 60%);"
-      ></div>
-      <div class="flex gap-4">
+    <div class="flex justify-center relative z-10 ">
+      <div class="absolute w-screen h-[200vh] overflow-hidden top-[-550px]">
+        <div
+          class="h-[200vh] z-10 max-w-screen overflow-x-hidden w-full scale-150 lg:scale-100 top-0"
+          style="background: radial-gradient(circle, rgba(2,246,124,.35) 0%, rgba(255,255,255,0) 60%);"
+        ></div>
+      </div>
+      <div class="flex gap-4 overflow-hidden">
         {Array(3)
           .fill(0)
           .map(() => (
