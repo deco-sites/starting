182e85cafdc32d0dfba06ba3e42a2d2d57a36b8c
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-08-08 17:51:56 -0300

    Update glossary's terms list design

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -3,6 +3,7 @@ import type { JSX } from "preact";
 
 export type AvailableIcons =
   | "ArrowRight"
+  | "right-arrow"
   | "Bag"
   | "Bars3"
   | "benchmark"
--- sections/GlossaryPostList.tsx
+++ sections/GlossaryPostList.tsx
@@ -1,6 +1,6 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
-import { ComponentChildren, Fragment } from "preact";
 import { BlogPost } from "apps/blog/types.ts";
+import Icon from "site/components/ui/Icon.tsx";
 
 export interface CTA {
   text?: string;
@@ -21,211 +21,84 @@ export interface Post {
 export interface Props {
   cta?: CTA;
   posts?: BlogPost[] | null;
-  pagination?: {
-    /**
-     * @title First page
-     * @description Leave it as 0 to start from the first page
-     */
-    page?: number;
-    /** @title items per page */
-    perPage?: number;
-  };
 }
 
-function Container({ children }: { children: ComponentChildren }) {
+function GlossaryItem({ title, link }: { title: string; link: string }) {
   return (
-    <div class="container lg:mx-auto lg:pb-14 lg:pt-0 mx-2 py-12 text-sm">
-      <div class="space-y-8">{children}</div>
-    </div>
+    <a
+      href={link}
+      class="flex items-center justify-between w-full md:w-60 py-2 px-4 gap-2 text-[#C9CFCF] hover:text-[#02F67C] hover:bg-[#0D171790] rounded-lg transition duration-300"
+    >
+      <span class="text-lg">{title}</span>
+      <Icon id="right-arrow" size={12} />
+    </a>
   );
 }
 
-export default function GlossaryPostList({
-  posts,
-  pagination: {
-    page = 0,
-    perPage = 60,
-  } = {},
-}: Props) {
-  const from = perPage * page;
-  const to = perPage * (page + 1);
-
-  const ContainerComponent = page === 0 ? Container : Fragment;
-  const usedFirstLetters = new Set();
-
+export default function GlossaryPostList({ posts }: Props) {
   posts?.sort((a, b) => {
     const titleA = a?.title?.toLowerCase().trim();
     const titleB = b?.title?.toLowerCase().trim();
     return titleA?.localeCompare(titleB);
   });
 
-  posts?.forEach((post) => {
-    const title = post?.title;
-    const firstLetter = title?.charAt(0)?.toUpperCase();
-    if (/[A-Za-z]/.test(firstLetter)) {
-      usedFirstLetters.add(firstLetter);
-    }
-  });
-
   function getFirstLetter(title: string) {
     return title?.charAt(0)?.toUpperCase();
   }
 
-  let currentLetter = "";
-  const alphabet = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
+  const alphabet = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  ); // Create list of characters
+
+  const filteredPosts = Object.fromEntries(
+    alphabet.map((char) => [
+      char,
+      posts?.filter((post) => getFirstLetter(post?.title) === char),
+    ])
+  );
 
   return (
     <section class="bg-black flex justify-center mb-20">
-      <ContainerComponent>
-        <div class="bg-black">
-          <div class="hidden md:flex justify-center mb-16">
-            {alphabet.map((letter) => {
-              const isUsed = Array.from(usedFirstLetters).includes(letter);
-              return (
-                <a
-                  class={`min-h-9 min-w-9 flex justify-center font-bold border border-transparent items-center transition duration-300 ${
-                    isUsed
-                      ? "font-bold text-white hover:border-[#02F67C40] hover:bg-[#02F67C30] rounded-lg"
-                      : "text-[#9499AD] cursor-default"
-                  }`}
-                  href={isUsed ? `#${letter}` : undefined}
-                >
-                  {letter}
-                </a>
-              );
-            })}
-          </div>
-
-          <div class="flex gap-10">
-            <div class="flex-col hidden md:flex min-w-[240px]">
-              <a href="/glossary" class="text-[#02F67C] hover:font-semibold">
-                All glossary terms
+      <div class="flex flex-col bg-black items-center w-full px-2">
+        <div class="flex overflow-scroll hidden-scroll mb-16 max-w-full w-fit">
+          {alphabet.map((letter) => {
+            return (
+              <a
+                class={`min-h-9 min-w-9 flex justify-center font-bold border border-transparent items-center transition duration-300 ${
+                  filteredPosts[letter]?.length
+                    ? "font-bold text-white hover:border-[#02F67C40] hover:bg-[#02F67C30] rounded-lg"
+                    : "text-[#9499AD] cursor-default"
+                }`}
+                href={filteredPosts[letter]?.length ? `#${letter}` : undefined}
+              >
+                {letter}
               </a>
-              {posts?.slice(from, to).map((post) => {
-                const title = post?.title;
-                const letter = getFirstLetter(title?.trim());
-                if (letter !== currentLetter) {
-                  currentLetter = letter;
-                  return (
-                    <div key={title} class="flex flex-col">
-                      <div class="w-10 h-[1px] bg-white my-4"></div>
-                      <a
-                        href={`/glossary/${post?.slug}`}
-                        class=" overflow-hidden  text-white hover:text-[#02F67C]"
-                      >
-                        {title}
-                      </a>
-                    </div>
-                  );
-                } else {
-                  return (
-                    <a
-                      key={title}
-                      href={`/glossary/${post?.slug}`}
-                      class="overflow-hidden  text-white hover:text-[#02F67C]"
-                    >
-                      {title}
-                    </a>
-                  );
-                }
-              })}
-            </div>
-            <div class="gap-8 grid grid-cols-1 w-full">
-              {posts?.slice(from, to).map((post) => {
-                const title = post?.title;
-                const letter = getFirstLetter(title?.trim());
-                if (letter !== currentLetter) {
-                  currentLetter = letter;
-                  return (
-                    <>
-                      {title && (
-                        <div key={title} class="max-w-[640px] w-full">
-                          <div class="flex flex-col gap-2 mb-6">
-                            <span
-                              id={currentLetter?.toUpperCase()}
-                              class="text-[#02F67C]"
-                            >
-                              {letter}
-                            </span>
-                            <div class="w-full h-[1px] bg-[#02F67C]"></div>
-                          </div>
-                          <a
-                            href={`/glossary/${post?.slug}`}
-                            class=" overflow-hidden"
-                          >
-                            <div class="p-6 space-y-4 bg-[#FFFFFF0D] border border-[#FFFFFF26] rounded-[20px]">
-                              <div class="space-y-2">
-                                <h3 class="text-2xl font-bold text-white">
-                                  {title}
-                                </h3>
-                                <p class="text-white">{post?.excerpt}</p>
-                              </div>
-                              <div class="flex flex-wrap gap-2">
-                                <span class="text-white">Read more</span>
-                              </div>
-                            </div>
-                          </a>
-                        </div>
-                      )}
-                    </>
-                  );
-                } else {
-                  return (
-                    <>
-                      {title && (
-                        <a
-                          key={title}
-                          href={`/glossary/${post?.slug}`}
-                          class="overflow-hidden max-w-[640px] w-full bg-[#FFFFFF0D] border border-[#FFFFFF26] rounded-[20px]"
-                        >
-                          <div class="p-6 space-y-4">
-                            <div class="space-y-2">
-                              <h3 class="text-2xl font-bold text-white">
-                                {title}
-                              </h3>
-                              <p class="text-white">{post?.excerpt}</p>
-                            </div>
-                            <div class="flex flex-wrap gap-2">
-                              <span class="text-white">Read more</span>
-                            </div>
-                          </div>
-                        </a>
-                      )}
-                    </>
-                  );
-                }
-              })}
-            </div>
-          </div>
+            );
+          })}
         </div>
-      </ContainerComponent>
+        <div class="flex flex-col gap-8 w-full max-w-[900px]">
+          {Object.entries(filteredPosts)
+            .filter(([, postList]) => postList && postList.length > 0)
+            .map(([char, currentPosts]) => (
+              <div class="flex flex-col md:flex-row gap-2">
+                <h2
+                  id={char}
+                  class="grow font-bold text-6xl p-2 pt-12 text-white"
+                >
+                  {char}
+                </h2>
+                <div class="grid gap-4 grid-cols-1 md:grid-cols-2 w-full md:w-fit">
+                  {currentPosts?.map((post) => (
+                    <GlossaryItem
+                      title={post?.title}
+                      link={`/glossary/${post?.slug}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </section>
   );
 }
