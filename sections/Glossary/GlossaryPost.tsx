e32c808b6a76100b252a1f3f6d2e2f4d495fe529
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-09-02 09:19:16 -0300

    Removes white background on code display

--- sections/Glossary/GlossaryPost.tsx
+++ sections/Glossary/GlossaryPost.tsx
@@ -38,7 +38,7 @@ const PARAGRAPH_STYLES = "[&_p]:leading-[150%] [&_*]:mb-4 text-[#cdcdcd]";
 const HEADING_STYLES =
   "[&>h1]:text-white [&>h1]:text-4xl [&>h1]:my-6 [&>h1]:font-bold [&>h2]:text-white [&>h2]:text-3xl [&>h2]:my-6 [&>h2]:font-bold [&>h1]:text-2xl [&>h1]:my-6 [&>h1]:font-bold [&>h3]:text-white [&>h3]:text-2xl [&>h3]:font-bold [&>h4]:text-xl [&>h4]:text-white [&>h4]:my-6 [&>h4]:font-bold [&>h5]:text-lg [&>h5]:text-white [&>h5]:my-6 [&>h5]:font-bold [&>h6]:text-white [&>h6]:my-6 [&>h6]:font-bold";
 const CODE_BLOCK_STYLES =
-  "[&>pre]:bg-gray-100 [&>pre]:text-white [&>pre]:p-4 [&>pre]:font-mono [&>pre]:text-sm [&>pre]:border [&>pre]:rounded-md [&>pre]:overflow-x-auto [&>code]:block [&>code]:w-full";
+  "[&>pre]:bg-gray-100 [&>pre]:text-white [&>pre]:p-4 [&>pre]:font-mono [&>pre]:text-sm [&>pre]:border [&>pre]:rounded-md [&>pre]:overflow-x-auto [&>pre]:bg-transparent [&>code]:block [&>code]:w-full";
 const IMAGE_STYLES = "[&_img]:rounded-2xl [&_img]:w-full [&_img]:my-12";
 const BLOCKQUOTE_STYLES =
   "[&>blockquote]:my-6 [&>blockquote]:border-l-2 [&>blockquote]:border-black [&>blockquote]:text-xl [&>blockquote]:italic [&>blockquote]:pl-6";
@@ -189,6 +189,8 @@ export default function GlossaryPost({ slug, posts, CTA, disclaimer }: Props) {
       prevPost = index - 1;
     }
   });
+
+  console.log(content);
 
   return (
     <div class="flex flex-col gap-12 md:gap-16 w-full container pt-28 md:pt-36 pb-16 justify-center">
